37f73dab972eaef1b06d0dc1e466d88528766b21
Zekeriya Koc <zekzekus@gmail.com>
2013-08-05 13:44:32 +0300

    FISH: added active virtualenv info in front of prompt

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -1,13 +1,16 @@
 function fish_prompt --description 'Write out the prompt'
-	
-  set last_status $status
+    if set -q VIRTUAL_ENV
+        echo -n -s (set_color -b blue white) (basename "$VIRTUAL_ENV") (set_color normal) " "
+    end
 
-  set_color $fish_color_cwd
-  printf '%s' (prompt_pwd)
-  set_color normal
+    set last_status $status
 
-  printf '%s ' (__fish_git_prompt)
+    set_color $fish_color_cwd
+    printf '%s' (prompt_pwd)
+    set_color normal
 
-  set_color normal
+    printf '%s ' (__fish_git_prompt)
+
+    set_color normal
 
 end
