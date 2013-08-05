a171df22720d32876f301af4b563465ed0f6a01a
Zekeriya Koc <zekzekus@gmail.com>
2013-08-05 12:03:27 +0300

    FISH: git prompt added

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -7,3 +7,29 @@ set -x EDITOR /usr/local/bin/vim
 . $CONFIGDIR/bundle/virtualfish/virtual.fish
 
 set fish_greeting
+
+# Fish git prompt
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showstashstate 'yes'
+set __fish_git_prompt_showupstream 'yes'
+set __fish_git_prompt_color_branch yellow
+
+# Status Chars
+set __fish_git_prompt_char_dirtystate '⚡'
+set __fish_git_prompt_char_stagedstate '→'
+set __fish_git_prompt_char_stashstate '↩'
+set __fish_git_prompt_char_upstream_ahead '↑'
+set __fish_git_prompt_char_upstream_behind '↓'
+
+
+function fish_prompt
+  set last_status $status
+
+  set_color $fish_color_cwd
+  printf '%s' (prompt_pwd)
+  set_color normal
+
+  printf '%s ' (__fish_git_prompt)
+
+  set_color normal
+end
