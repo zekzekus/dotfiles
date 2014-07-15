5dd22b8b141c195d2f53d70e1deb7f4149097806
Zekeriya Koc <zekzekus@gmail.com>
2014-07-15 11:43:32 +0300

    FISH: prompt configured

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,53 +1,17 @@
+# PATH modifications. last item has the highest priority
 set --export PATH (brew --prefix)/bin $PATH
 set --export PATH /usr/local/share/npm/bin $PATH
 set --export PATH $HOME/bin $PATH
 
+# my favorite text editor
 set -U EDITOR vim
 
+# python virtualenv tool
+set -g VIRTUALFISH_COMPAT_ALIASES
 . ~/.config/fish/plugins/virtualfish.git/virtual.fish
 
+# color codes for prompt functions
 set -g fish_color_user magenta
 set -g fish_color_host yellow
-set -g fish_prompt_git_status_git_dir '⚒'  
+set -g fish_prompt_git_status_git_dir '⚒'
 set -g fish_prompt_git_remote_space ' '
-
-function fish_prompt --description 'Write out the prompt'
-  set -l last_status $status
-
-  if set -q VIRTUAL_ENV
-    echo -n -s (set_color -b blue white) "(" (basename "$VIRTUAL_ENV") ")" (set_color normal) " "
-  end
-
-  # User
-  set_color $fish_color_user
-  echo -n (whoami)
-  set_color normal
-
-  echo -n ':'
-
-  # PWD
-  set_color $fish_color_cwd
-  echo -n (prompt_pwd)
-  set_color normal
-
-  printf '%s ' (__informative_git_prompt)
-
-  if not test $last_status -eq 0
-    set_color $fish_color_error
-  end
-
-  echo -n '$ '
-  
-  set_color $fish_color_normal
-
-end
-
-function fish_right_prompt -d "Write out the right prompt"
-
-  # Time
-  set_color -o black
-  echo (date +%R)
-  set_color $fish_color_normal
-
-end
-
---
+++ config/fish/functions/fish_prompt.fish
@@ -0,0 +1,27 @@
+function fish_prompt --description 'Write out the prompt'
+  set -l last_status $status
+
+  if set -q VIRTUAL_ENV
+    echo -n -s (set_color -b blue white) "(" (basename "$VIRTUAL_ENV") ")" (set_color normal) " "
+  end
+
+  echo -n ':'
+
+  # PWD
+  set_color $fish_color_cwd
+  echo -n (basename (prompt_pwd))
+  set_color normal
+
+  echo -n ':'
+
+  printf '%s ' (__informative_git_prompt)
+
+  if not test $last_status -eq 0
+    set_color $fish_color_error
+  end
+
+  echo -n '> '
+
+  set_color $fish_color_normal
+
+end
