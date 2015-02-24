f15f42d3fcfff6f631e73d23fb0cdad75d1f0ad6
Zekeriya Koc <zekzekus@gmail.com>
2015-02-25 00:06:34 +0200

    CLOJURE: added leiningen profile file

---
+++ lein/profiles.clj
@@ -0,0 +1,5 @@
+{:user
+ {:plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"] ]
+  :dependencies [[jonase/eastwood "0.2.1"
+                  :exclusions  [org.clojure/clojure]]
+                 [cljfmt "0.1.7"]]}}
