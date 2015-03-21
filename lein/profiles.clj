1e61b22e800d0ca1c39a9e506d066a36e219c803
Zekeriya Koc <zekzekus@gmail.com>
2015-03-21 21:38:45 +0200

    LEIN: profile updated for minimal

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,5 +1,4 @@
-{:user
- {:plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"] ]
-  :dependencies [[jonase/eastwood "0.2.1"
-                  :exclusions  [org.clojure/clojure]]
-                 [cljfmt "0.1.7"]]}}
+{:user {:plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"]
+                  [refactor-nrepl "0.3.0-SNAPSHOT"]]
+        :dependencies [[alembic "0.3.2"]
+                       [org.clojure/tools.nrepl "0.2.7"]]}}
