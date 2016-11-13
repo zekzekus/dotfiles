031d7b33da972d73cfb848edca931d3cbd1493ff
Zekeriya Koc <zekzekus@gmail.com>
2016-11-13 19:23:04 +0300

    GENERAL: removed redundant conf sections

--- lein/profiles.clj
+++
@@ -1,4 +0,0 @@
-{:user {:plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"]
-                  [refactor-nrepl "0.3.0-SNAPSHOT"]]
-        :dependencies [[alembic "0.3.2"]
-                       [org.clojure/tools.nrepl "0.2.7"]]}}
