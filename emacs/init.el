92254d07d5f5ca84f66182695b8ac6ea87488cf3
Zekeriya Koc <zekzekus@gmail.com>
2018-09-09 19:50:31 +0300

    EMACS: added parinfer package

--- emacs/init.el
+++ emacs/init.el
@@ -184,6 +184,23 @@
 
 (use-package haskell-mode :ensure t)
 
+(use-package parinfer
+  :ensure t
+  :bind
+  (("C-," . parinfer-toggle-mode))
+  :init
+  (progn
+    (setq parinfer-extensions
+	  '(defaults       ; should be included.
+	     evil           ; If you use Evil.
+	     paredit        ; Introduce some paredit commands.
+	     smart-tab      ; C-b & C-f jump positions and smart shift with tab & S-tab.
+	     smart-yank))   ; Yank behavior depend on mode.
+    (add-hook 'clojure-mode-hook #'parinfer-mode)
+    (add-hook 'emacs-lisp-mode-hook #'parinfer-mode)
+    (add-hook 'common-lisp-mode-hook #'parinfer-mode)
+    (add-hook 'lisp-mode-hook #'parinfer-mode)))
+
 (use-package evil-magit
   :ensure t
   :config
