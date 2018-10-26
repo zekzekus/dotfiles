5fe8bc3273539ee4dcf9157373374cb7c48859df
Zekeriya Koc <zekzekus@gmail.com>
2018-10-26 16:15:53 +0300

    EMACS: added evil collection package

--- emacs/init.el
+++ emacs/init.el
@@ -61,8 +61,16 @@
 
 (use-package evil
   :ensure t
+  :init
+  (setq evil-want-keybinding nil)
   :config
   (evil-mode 1))
+
+(use-package evil-collection
+  :after evil
+  :ensure t
+  :config
+  (evil-collection-init))
 
 (use-package evil-commentary
   :ensure t
