b062f7cbd9ce912290485945253ccdc4533c24ef
Zekeriya Koc <zekzekus@gmail.com>
2018-10-27 20:21:29 +0300

    EMACS: added javascript react mode

--- emacs/init.el
+++ emacs/init.el
@@ -170,10 +170,10 @@
   :diminish company-mode
   :init
   (global-company-mode)
-  (setq company-tooltip-limit 20)                      ; bigger popup window
-  (setq company-idle-delay .3)                         ; decrease delay before autocompletion popup shows
-  (setq company-echo-delay 0)                          ; remove annoying blinking
-  (setq company-begin-commands '(self-insert-command))) ; start autocompletion only after typing
+  (setq company-tooltip-limit 20)
+  (setq company-idle-delay .3)
+  (setq company-echo-delay 0)
+  (setq company-begin-commands '(self-insert-command)))
 
 (use-package anaconda-mode
   :ensure t
@@ -222,6 +222,11 @@
   :ensure t
   :config
   (add-hook 'eval-expression-minibuffer-setup-hook #'eldoc-mode))
+
+(use-package rjsx-mode
+  :ensure t
+  :config
+  (add-to-list 'auto-mode-alist '("components\\/.*\\.js\\'" . rjsx-mode)))
 
 (use-package org :ensure t)
 
