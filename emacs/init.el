658af8a2856a39b01671c62ee7d52aad2b1c267e
Zekeriya Koc <zekzekus@gmail.com>
2018-11-09 16:30:44 +0300

    EMACS: added missing pyvenv package

--- emacs/init.el
+++ emacs/init.el
@@ -219,6 +219,9 @@
   (add-hook 'python-mode-hook 'anaconda-mode)
   (add-hook 'python-mode-hook 'anaconda-eldoc-mode))
 
+(use-package pyvenv
+  :ensure t)
+
 (use-package company-anaconda
   :ensure t
   :after company
