08e09ccb57ddc7f5a8168822cf6762c028c91ecf
Zekeriya Koc <zekzekus@gmail.com>
2018-11-16 13:05:16 +0300

    EMACS: added custom.el file to repo to keep track of customizations

---
+++ emacs/custom.el
@@ -0,0 +1,15 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("cc0dbb53a10215b696d391a90de635ba1699072745bf653b53774706999208e3" default))
+ '(package-selected-packages
+   '(which-key use-package tron-theme telephone-line tao-theme smex smart-mode-line slime rjsx-mode racer pyvenv parinfer paredit nord-theme minimal-theme markdown-mode intero general exec-path-from-shell evil-org evil-magit evil-escape evil-commentary evil-collection diminish counsel-projectile company-go company-anaconda cider)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
