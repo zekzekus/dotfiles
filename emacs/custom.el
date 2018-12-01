b8a7139ff7844af42d54a90f48bd0b7c789f4d2f
Zekeriya Koc <zekzekus@gmail.com>
2018-12-02 00:40:30 +0300

    EMACS: customized org mode faces a bit

--- emacs/custom.el
+++ emacs/custom.el
@@ -3,18 +3,21 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
  '(custom-safe-themes
    '("cc0dbb53a10215b696d391a90de635ba1699072745bf653b53774706999208e3" default))
+ '(hl-sexp-background-color "#efebe9")
  '(package-selected-packages
    '(use-package darktooth-theme doom-modeline ace-window smooth-scrolling shell-pop evil evil-collection evil-commentary magit evil-magit diminish ivy ivy-hydra hydra counsel swiper smex exec-path-from-shell which-key projectile counsel-projectile evil-escape paredit parinfer company anaconda-mode pyvenv company-anaconda rust-mode racer haskell-mode intero go-mode company-go cider slime rjsx-mode org evil-org markdown-mode flycheck restclient general))
  '(shell-pop-full-span t)
- '(shell-pop-shell-type
-   '("ansi-term" "*ansi-term*"
-	 (lambda nil
-	   (ansi-term shell-pop-term-shell)))))
+ '(shell-pop-shell-type '("ansi-term" "*ansi-term*")))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(org-checkbox-statistics-done ((t (:inherit org-done :box (:line-width 1 :color "grey75" :style released-button)))))
+ '(org-checkbox-statistics-todo ((t (:inherit org-todo :box (:line-width 1 :color "grey75" :style released-button)))))
+ '(org-level-1 ((t (:background "gray18" :foreground "#FE8019" :overline "grey75"))))
+ '(org-level-2 ((t (:background "gray18" :foreground "#FE8019" :overline "grey75")))))
