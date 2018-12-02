9427223862bdf19c9d282248ae919b29d6f6443f
Zekeriya Koc <zekzekus@gmail.com>
2018-12-02 18:54:03 +0300

    EMACS: added new installed packages to selected packages list

--- emacs/custom.el
+++ emacs/custom.el
@@ -9,7 +9,7 @@
    '("cc0dbb53a10215b696d391a90de635ba1699072745bf653b53774706999208e3" default))
  '(hl-sexp-background-color "#efebe9")
  '(package-selected-packages
-   '(use-package darktooth-theme doom-modeline ace-window smooth-scrolling shell-pop evil evil-collection evil-commentary magit evil-magit diminish ivy ivy-hydra hydra counsel swiper smex exec-path-from-shell which-key projectile counsel-projectile evil-escape paredit parinfer company anaconda-mode pyvenv company-anaconda rust-mode racer haskell-mode intero go-mode company-go cider slime rjsx-mode org evil-org markdown-mode flycheck restclient general))
+   '(use-package darktooth-theme doom-modeline ace-window smooth-scrolling shell-pop evil evil-collection evil-commentary magit evil-magit diminish ivy ivy-hydra hydra counsel swiper smex exec-path-from-shell which-key projectile counsel-projectile evil-escape paredit parinfer company anaconda-mode pyvenv company-anaconda rust-mode racer haskell-mode intero go-mode company-go cider slime rjsx-mode org evil-org markdown-mode flycheck restclient treemacs treemacs-evil treemacs-projectile general))
  '(shell-pop-full-span t)
  '(shell-pop-shell-type
    '("ansi-term" "*ansi-term*"
