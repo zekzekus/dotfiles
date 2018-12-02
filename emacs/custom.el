9fdd028be2204eb9031fffc6af77461eb8bd188e
Zekeriya Koc <zekzekus@gmail.com>
2018-12-02 18:48:03 +0300

    EMACS: fixed terminal settings

--- emacs/custom.el
+++ emacs/custom.el
@@ -11,7 +11,10 @@
  '(package-selected-packages
    '(use-package darktooth-theme doom-modeline ace-window smooth-scrolling shell-pop evil evil-collection evil-commentary magit evil-magit diminish ivy ivy-hydra hydra counsel swiper smex exec-path-from-shell which-key projectile counsel-projectile evil-escape paredit parinfer company anaconda-mode pyvenv company-anaconda rust-mode racer haskell-mode intero go-mode company-go cider slime rjsx-mode org evil-org markdown-mode flycheck restclient general))
  '(shell-pop-full-span t)
- '(shell-pop-shell-type '("ansi-term" "*ansi-term*")))
+ '(shell-pop-shell-type
+   '("ansi-term" "*ansi-term*"
+     (lambda nil
+       (ansi-term shell-pop-term-shell)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
