b2dc3d42ff6f4ccc344ae13c69442f1c4f2cede6
Zekeriya Koc <zekzekus@gmail.com>
2019-02-21 09:47:34 +0300

    FISH: set vi mode on shell

--- fish/config.fish
+++ fish/config.fish
@@ -40,3 +40,5 @@ end
 status --is-interactive; and source (nodenv init -|psub)
 set -g fish_user_paths "/usr/local/opt/icu4c/bin" $fish_user_paths
 set -g fish_user_paths "/usr/local/opt/icu4c/sbin" $fish_user_paths
+
+fish_vi_key_bindings
