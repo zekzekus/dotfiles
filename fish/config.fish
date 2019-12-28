3bb1fd2b6de5f64f392a75bbb0c86426e9519bba
Zekeriya Koç <zekus@Zekeriyas-MacBook-Pro.local>
2019-12-28 12:27:35 +0300

    FISH: installed fisher plugin manager plus bass plugin

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,9 @@
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher
+end
+
 set --export GOROOT /usr/local/opt/go/libexec
 set --export GOPATH $HOME/go
 
