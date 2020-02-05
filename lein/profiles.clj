3692e75069147294906382c044a4038b4de92a4d
Zekeriya Koc <zekzekus@gmail.com>
2020-02-05 14:21:34 +0300

    LEIN: updated simple lein profiles file

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,4 +1,1 @@
-{:user {:plugins [[cider/cider-nrepl "RELEASE"]
-                  [refactor-nrepl "RELEASE"]]
-        :dependencies [[cljfmt "RELEASE"]]
-        :repl-options {:init (require 'cljfmt.core)}}}
+{:user {:plugins [[cider/cider-nrepl "0.23.0"]]}}
