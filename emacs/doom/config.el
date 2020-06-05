134b7dcdc4582a1a3366886bef393af50acafb9a
Zekeriya Koc <zekzekus@gmail.com>
2020-06-05 13:30:43 +0300

    EMACS: this color thing will take lots of commits

--- emacs/doom/config.el
+++ emacs/doom/config.el
@@ -23,7 +23,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. These are the defaults.
-(setq doom-theme 'doom-nord-light)
+(setq doom-theme 'nordless)
 
 ;; If you intend to use org, it is recommended you change this!
 (setq org-directory "~/org")
