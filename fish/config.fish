28bb8f1c30d6df704abbc9c63711911c072e7f9c
Zekeriya Koc <zekzekus@gmail.com>
2020-06-21 15:35:25 +0300

    FISH:BASH: Make these two similar and minor cleanup

--- fish/config.fish
+++ fish/config.fish
@@ -7,15 +7,12 @@ end
 set --export GOROOT /usr/local/opt/go/libexec
 set --export GOPATH $HOME/go
 
-# PATH modifications. last item has the highest priority
 set --export PATH /usr/local/bin $PATH
 set --export PATH /usr/local/sbin $PATH
 set --export PATH /usr/local/opt/go/libexec/bin $PATH
-set --export PATH $HOME/.gem/ruby/2.6.0/bin $PATH
 set --export PATH $HOME/.cargo/bin $PATH
 set --export PATH $GOPATH/bin $PATH
 set --export PATH $HOME/.local/bin $PATH
-set --export PATH $HOME/.ghcup/bin $PATH
 set --export PATH $HOME/.cabal/bin $PATH
 set --export PATH $HOME/bin $PATH
 set -g fish_user_paths "/usr/local/opt/icu4c/bin" $fish_user_paths
@@ -33,22 +30,21 @@ set --export LC_ALL "en_US.UTF-8"
 set --export EDITOR nvim
 set --export MANPAGER "nvim -c 'set ft=man' -"
 set --export PGDATA /usr/local/var/postgres
-
+set --export RUBY_CONFIGURE_OPTS --with-openssl-dir=(brew --prefix openssl@1.1)
 set --export RUST_SRC_PATH (rustc --print sysroot)/lib/rustlib/src/rust/src
 
 alias mux='tmuxinator'
-
-set --export RUBY_CONFIGURE_OPTS --with-openssl-dir=(brew --prefix openssl@1.1)
-rbenv init - | source
-nodenv init - | source
-eval (direnv hook fish)
-python3 -m virtualfish | source
 
 set --export FZF_DEFAULT_COMMAND 'rg --files --hidden --follow --glob "!.git/*"'
 set --export FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
 set --export FZF_ALT_C_COMMAND 'bfs -type d -nohidden'
 
 set --export HOMEBREW_NO_INSTALL_CLEANUP 1
+
+rbenv init - | source
+nodenv init - | source
+eval (direnv hook fish)
+python3 -m virtualfish | source
 
 if test -e ~/.nix-profile/etc/profile.d/nix.sh
   bass source ~/.nix-profile/etc/profile.d/nix.sh
