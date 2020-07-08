5fc84c17287170c3e11c032a12afa2595948aa65
Zekeriya Koc <zekzekus@gmail.com>
2020-07-08 20:48:40 +0300

    EMACS: periodic color theme bounce

--- emacs/doom/config.el
+++ emacs/doom/config.el
@@ -26,7 +26,7 @@
 ;; `load-theme' function. These are the defaults.
 (load! "menguless-theme")
 
-(setq doom-theme 'doom-nord-light)
+(setq doom-theme 'parchment)
 (setq display-line-numbers-type nil)
 (setq mac-command-modifier 'meta
       mac-option-modifier 'none)
