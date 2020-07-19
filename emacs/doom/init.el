4df824dc9b9163ca6d128b3d72629340f6f50665
Zekeriya Koc <zekzekus@gmail.com>
2020-07-19 20:59:19 +0300

    EMACS: added rails flag to ruby module

--- emacs/doom/init.el
+++ emacs/doom/init.el
@@ -156,7 +156,9 @@
        ;;racket            ; a DSL for DSLs
        rest              ; Emacs as a REST client
        rst               ; ReST in peace
-       (ruby +rbenv) ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
+       (ruby
+        +rbenv
+        +rails)
        rust        ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        scala             ; java, but good
        ;;scheme            ; a fully conniving family of lisps
