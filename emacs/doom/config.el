dd263876bd65fea7f98d50081f7ab5baea725c4f
Zekeriya Koc <zekzekus@gmail.com>
2020-09-24 00:12:35 +0300

    EMACS: updated my email address

--- emacs/doom/config.el
+++ emacs/doom/config.el
@@ -7,7 +7,7 @@
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
 (setq user-full-name "Zekeriya Koc"
-      user-mail-address "zekzekus@gmail.com")
+      user-mail-address "zekeriya@hey.com")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
 ;; are the three important ones:
