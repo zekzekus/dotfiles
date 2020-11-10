8095613a7a08483e7068215ef11ff9a2c9b91ee5
Zekeriya Koc <zekzekus@gmail.com>
2020-11-10 23:41:10 +0300

    EMACS: swapped terminal emulation packages

--- emacs/doom/init.el
+++ emacs/doom/init.el
@@ -73,8 +73,8 @@
        :term
        ;; eshell            ; a consistent, cross-platform shell (WIP)
        ;; shell             ; a terminal REPL for Emacs
-       ;; term              ; terminals in Emacs
-       vterm             ; another terminals in Emacs
+       term              ; terminals in Emacs
+       ;; vterm             ; another terminals in Emacs
 
        :checkers
        syntax              ; tasing you for every semicolon you forget
