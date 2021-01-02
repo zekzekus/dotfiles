990075ec16cea205f6ab1d6f5300c46822a39ab6
Zekeriya Koc <zekzekus@gmail.com>
2021-01-02 21:42:20 +0300

    FISH: removed golang based old configuration

--- fish/config.fish
+++ fish/config.fish
@@ -4,14 +4,9 @@ if not functions -q fisher
     fish -c fisher
 end
 
-set --export GOROOT /usr/local/opt/go/libexec
-set --export GOPATH $HOME/go
-
 set --export PATH /usr/local/bin $PATH
 set --export PATH /usr/local/sbin $PATH
-set --export PATH /usr/local/opt/go/libexec/bin $PATH
 set --export PATH $HOME/.cargo/bin $PATH
-set --export PATH $GOPATH/bin $PATH
 set --export PATH $HOME/.local/bin $PATH
 set --export PATH $HOME/.cabal/bin $PATH
 set --export PATH $HOME/bin $PATH
