bfdec599eb591a45cb6e9b5768fc003aabc53926
Zekeriya Koc <zekzekus@gmail.com>
2021-03-01 20:48:37 +0300

    FISH: added virtualfish

---
+++ fish/conf.d/virtualfish-loader.fish
@@ -0,0 +1,4 @@
+set -g VIRTUALFISH_VERSION 2.5.1
+set -g VIRTUALFISH_PYTHON_EXEC /usr/local/opt/python@3.9/bin/python3.9
+source /usr/local/lib/python3.9/site-packages/virtualfish/virtual.fish
+emit virtualfish_did_setup_plugins
