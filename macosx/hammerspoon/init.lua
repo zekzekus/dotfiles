0f936701da6e56067959d8d3044c810df97eb69c
Zekeriya Koc <zekzekus@gmail.com>
2021-06-30 10:12:27 +0300

    HAMMERSPOON: removed evernote from auto launch

--- macosx/hammerspoon/init.lua
+++ macosx/hammerspoon/init.lua
@@ -191,7 +191,6 @@ hs.hotkey.bind(hyper, "a", function()
   hs.application.launchOrFocus("iTerm")
   hs.application.launchOrFocus("Slack")
   hs.application.launchOrFocus("Mail")
-  hs.application.launchOrFocus("Evernote")
 end)
 -- hs.hotkey.bind(hyper, "r", function() hs.application.launchOrFocus("Roam Research") end)
 
