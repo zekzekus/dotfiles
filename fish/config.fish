d454e89da8ef4a1185a0ff9ed5a8596d25ba0c04
Zekeriya Koc <zekzekus@gmail.com>
2021-07-10 11:14:31 +0300

    FISH: uninstalled old fisher version and installed latest

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,3 @@
-if not functions -q fisher
-    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
-    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
-    fish -c fisher
-end
-
 #set --export GOROOT /usr/local/opt/go/libexec
 #set --export GOPATH $HOME/go
 set --export PATH /usr/local/bin $PATH
