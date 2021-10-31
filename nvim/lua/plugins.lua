00cf4dd7b406c931c6b9812314346c46161dfa9e
Zekeriya Koc <zekzekus@gmail.com>
2021-10-31 12:26:28 +0300

    NVIM: retab lua files to be more consistent

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -5,85 +5,85 @@ if not packer then
 end
 
 packer.startup(function(use)
-	use 'wbthomason/packer.nvim'
+  use 'wbthomason/packer.nvim'
 
-	use 'tpope/vim-sleuth'
-	use 'tpope/vim-repeat'
-	use 'tpope/vim-surround'
-	use 'jiangmiao/auto-pairs'
-	use 'junegunn/fzf.vim'
-	use '/usr/local/opt/fzf'
-	use 'tpope/vim-unimpaired'
-	use 'farmergreg/vim-lastplace'
-	use 'christoomey/vim-tmux-navigator'
-	use 'sheerun/vim-polyglot'
-	use 'tpope/vim-fugitive'
-	use 'tpope/vim-commentary'
-	use 'LionC/nest.nvim'
-	use 'tpope/vim-dispatch'
-	use 'radenling/vim-dispatch-neovim'
+  use 'tpope/vim-sleuth'
+  use 'tpope/vim-repeat'
+  use 'tpope/vim-surround'
+  use 'jiangmiao/auto-pairs'
+  use 'junegunn/fzf.vim'
+  use '/usr/local/opt/fzf'
+  use 'tpope/vim-unimpaired'
+  use 'farmergreg/vim-lastplace'
+  use 'christoomey/vim-tmux-navigator'
+  use 'sheerun/vim-polyglot'
+  use 'tpope/vim-fugitive'
+  use 'tpope/vim-commentary'
+  use 'LionC/nest.nvim'
+  use 'tpope/vim-dispatch'
+  use 'radenling/vim-dispatch-neovim'
 
-	use { 'preservim/tagbar',									cmd 		 = { 'TagbarToggle' } }
-	use { 'ellisonleao/glow.nvim',            cmd 		 = { 'Glow' } }
-	use { 'Shougo/junkfile.vim', 							cmd 		 = { 'JunkfileOpen' } }
-	use { 'szw/vim-smartclose', 							cmd 		 = { 'SmartClose' } }
-	use { 'AndrewRadev/linediff.vim', 				cmd 		 = { 'Linediff' } }
-	use { 'tpope/vim-dadbod', 								cmd 		 = { 'DB' } }
-	use { 'nvim-treesitter/nvim-treesitter', 	run 		 = { ':TSUpdate' } }
-	use { 'eraserhd/parinfer-rust', 					run 		 = {  'cargo build --release' }, ft = { 'clojure' } }
-	use { 'clojure-vim/vim-jack-in', 					ft  		 = { 'clojure' } }
-	use { 'tpope/vim-jdaddy', 								ft  		 = { 'json' } }
-	use { 'tpope/vim-fireplace', 							ft  		 = { 'clojure' } }
-	use { 'mcchrish/zenbones.nvim', 					requires = { 'rktjmp/lush.nvim' } }
-	use { 'alvarosevilla95/luatab.nvim', 			requires = { 'kyazdani42/nvim-web-devicons' } }
+  use { 'preservim/tagbar',                 cmd      = { 'TagbarToggle' } }
+  use { 'ellisonleao/glow.nvim',            cmd      = { 'Glow' } }
+  use { 'Shougo/junkfile.vim',              cmd      = { 'JunkfileOpen' } }
+  use { 'szw/vim-smartclose',               cmd      = { 'SmartClose' } }
+  use { 'AndrewRadev/linediff.vim',         cmd      = { 'Linediff' } }
+  use { 'tpope/vim-dadbod',                 cmd      = { 'DB' } }
+  use { 'nvim-treesitter/nvim-treesitter',  run      = { ':TSUpdate' } }
+  use { 'eraserhd/parinfer-rust',           run      = { 'cargo build --release' }, ft = { 'clojure' } }
+  use { 'clojure-vim/vim-jack-in',          ft       = { 'clojure' } }
+  use { 'tpope/vim-jdaddy',                 ft       = { 'json' } }
+  use { 'tpope/vim-fireplace',              ft       = { 'clojure' } }
+  use { 'mcchrish/zenbones.nvim',           requires = { 'rktjmp/lush.nvim' } }
+  use { 'alvarosevilla95/luatab.nvim',      requires = { 'kyazdani42/nvim-web-devicons' } }
 
 
-	use {
-		'lewis6991/gitsigns.nvim',
-		config = function() default_config('gitsigns') end,
-	}
-
-	use {
-		'TimUntersberger/neogit',
-		requires = 'nvim-lua/plenary.nvim',
-		config = function() default_config('neogit') end,
+  use {
+    'lewis6991/gitsigns.nvim',
+    config = function() default_config('gitsigns') end,
   }
 
-	use {
-		'nvim-telescope/telescope.nvim',
-		requires = {
-			{'nvim-lua/popup.nvim'},
-			{'nvim-lua/plenary.nvim'},
-			{'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
-		},
-		config = function() require('config.telescope') end,
-	}
+  use {
+    'TimUntersberger/neogit',
+    requires = 'nvim-lua/plenary.nvim',
+    config = function() default_config('neogit') end,
+  }
 
-	use {
-			'kyazdani42/nvim-tree.lua',
-			requires = 'kyazdani42/nvim-web-devicons',
-			config = function() default_config('nvim-tree') end,
-	}
+  use {
+    'nvim-telescope/telescope.nvim',
+    requires = {
+      {'nvim-lua/popup.nvim'},
+      {'nvim-lua/plenary.nvim'},
+      {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+    },
+    config = function() require('config.telescope') end,
+  }
 
-	use {
-		'neovim/nvim-lspconfig',
-		config = function() require('config.lspconfig') end,
-		after = { 'cmp-nvim-lsp', 'nvim-lsp-installer' },
-		requires = { { 'williamboman/nvim-lsp-installer' } },
-	}
+  use {
+      'kyazdani42/nvim-tree.lua',
+      requires = 'kyazdani42/nvim-web-devicons',
+      config = function() default_config('nvim-tree') end,
+  }
 
-	use {
-		'hrsh7th/nvim-cmp',
-		requires = {
-				{ 'hrsh7th/cmp-nvim-lsp', after = 'nvim-cmp', },
-		},
-		config = function() require('config.cmp') end,
-	}
+  use {
+    'neovim/nvim-lspconfig',
+    config = function() require('config.lspconfig') end,
+    after = { 'cmp-nvim-lsp', 'nvim-lsp-installer' },
+    requires = { { 'williamboman/nvim-lsp-installer' } },
+  }
 
-	use {
-		'scalameta/nvim-metals',
-		requires = { "nvim-lua/plenary.nvim" },
-		config = function() require('config.nvim-metals') end,
-	}
+  use {
+    'hrsh7th/nvim-cmp',
+    requires = {
+        { 'hrsh7th/cmp-nvim-lsp', after = 'nvim-cmp', },
+    },
+    config = function() require('config.cmp') end,
+  }
+
+  use {
+    'scalameta/nvim-metals',
+    requires = { "nvim-lua/plenary.nvim" },
+    config = function() require('config.nvim-metals') end,
+  }
 
 end)
--- nvim/lua/utils.lua
+++ nvim/lua/utils.lua
@@ -1,19 +1,19 @@
 _G.prequire = function(plugin, verbose)
-    local present, plug = pcall(require, plugin)
-    if present then
-        return plug
-    end
-    local errmsg = string.format("Could not load %s", plugin)
-    if verbose then
-        errmsg = string.format("%s\nError:%s", plug)
-    end
-    print(errmsg)
+  local present, plug = pcall(require, plugin)
+  if present then
+    return plug
+  end
+  local errmsg = string.format("Could not load %s", plugin)
+  if verbose then
+    errmsg = string.format("%s\nError:%s", plug)
+  end
+  print(errmsg)
 end
 
 _G.default_config = function(plugin)
-    local plug = prequire(plugin)
-    if not plug then
-        return
-    end
-    plug.setup({})
+  local plug = prequire(plugin)
+  if not plug then
+    return
+  end
+  plug.setup({})
 end
