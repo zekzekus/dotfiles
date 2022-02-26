e0b4974a4bff05c59113d2c5d1b862c4859a017a
Zekeriya Koc <zekzekus@gmail.com>
2022-02-26 19:17:07 +0300

    NVIM: let's try GTD with neorg

--- nvim/lua/config/neorg.lua
+++ nvim/lua/config/neorg.lua
@@ -7,10 +7,20 @@ end
 neorg.setup {
   load = {
     ["core.defaults"] = {},
-    -- ["core.gtd.base"] = {},
+    ["core.norg.dirman"] = {
+      config = {
+        workspaces = {
+          gtd = "~/norg",
+        },
+      },
+    },
+    ["core.gtd.base"] = {
+      config = {
+        workspace = "gtd",
+      },
+    },
     -- ["core.norg.completion"] = {},
     ["core.norg.concealer"] = {},
-    ["core.norg.dirman"] = {},
     ["core.norg.journal"] = {},
     ["core.norg.qol.toc"] = {},
     -- ["core.presenter"] = {},
