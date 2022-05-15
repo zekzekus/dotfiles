91306a6bac9eade82e0fc55f6fb67642f5c8a333
Zekeriya Koc <zekzekus@gmail.com>
2022-05-16 00:27:26 +0300

    EMACS: added java config enabled

--- emacs/doom/custom.el
+++ emacs/doom/custom.el
@@ -3,24 +3,31 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(neuron-default-tags '("stub"))
- '(neuron-default-zettelkasten-directory "/Users/zekus/zettelkasten")
- '(neuron-tag-specific-title-faces '(("stub" neuron-stub-face)))
+ '(neuron-default-tags '("stub") t)
+ '(neuron-default-zettelkasten-directory "/Users/zekus/zettelkasten" t)
+ '(neuron-tag-specific-title-faces '(("stub" neuron-stub-face)) t)
  '(safe-local-variable-values
    '((Base . 10)
      (Syntax . ANSI-Common-Lisp)
      (checkdoc-package-keywords-flag)
      (cider-cljs-repl-types
-      (edge "(do (require 'dev-extras) ((resolve 'dev-extras/cljs-repl)))"))
+           (edge "(do (require 'dev-extras) ((resolve 'dev-extras/cljs-repl)))"))
      (cider-default-cljs-repl . edge)
      (cider-clojure-cli-global-options . "-A:dev:build:dev/build")
      (cider-clojure-cli-global-options . "-A:dev")
      (cider-repl-init-code "(dev)")
      (cider-ns-refresh-after-fn . "dev-extras/resume")
-     (cider-ns-refresh-before-fn . "dev-extras/suspend"))))
-(custom-set-faces
+     (cider-ns-refresh-before-fn . "dev-extras/suspend")))
+ '(warning-suppress-log-types '((find-file-hook) (find-file-hook)))
+ '(warning-suppress-types '((find-file-hook))))
+(custom-set-faces)
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ 
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ 
