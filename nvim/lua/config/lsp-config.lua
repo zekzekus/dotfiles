bebec0e5c20ac848311f20f9464e6e751fde2b72
Zekeriya Koc <zekzekus@gmail.com>
2022-07-05 16:55:22 +0300

    NVIM: removed clojure lsp because fireplace is enough

--- nvim/lua/config/lsp-config.lua
+++ nvim/lua/config/lsp-config.lua
@@ -56,7 +56,7 @@ lspconfig["denols"].setup {
   capabilities = cmplsp.update_capabilities(vim.lsp.protocol.make_client_capabilities())
 }
 
-local servers = { "gopls", "jdtls", "jsonls", "sumneko_lua", "pyright", "rust_analyzer", "clojure_lsp", "elixirls" }
+local servers = { "gopls", "jdtls", "jsonls", "sumneko_lua", "pyright", "rust_analyzer", "elixirls" }
 for _, server in pairs(servers) do
   lspconfig[server].setup {
     autostart = true,
