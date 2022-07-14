a085abfd3f833589f7926429c94fd922a7fd4687
Zekeriya Koc <zekzekus@gmail.com>
2022-07-14 11:40:03 +0300

    EMACS: added lsp flag to rust module

--- emacs/doom/init.el
+++ emacs/doom/init.el
@@ -175,7 +175,7 @@
        rest              ; Emacs as a REST client
        rst               ; ReST in peace
        (ruby +rails)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
-       rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
+       (rust +lsp)              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        (scala             ; java, but good
         +lsp)
        ;;scheme            ; a fully conniving family of lisps
