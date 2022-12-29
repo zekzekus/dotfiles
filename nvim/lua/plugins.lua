48d999c5dbf7955e9be14997f44ec07e3023ef96
Zekeriya Koc <zekzekus@gmail.com>
2022-12-29 21:46:02 +0300

    NVIM: removed buffer auto completions

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -96,7 +96,6 @@ packer.startup(function(use)
       { 'tami5/lspsaga.nvim' },
 
       {'hrsh7th/nvim-cmp'},
-      {'hrsh7th/cmp-buffer'},
       {'hrsh7th/cmp-path'},
       {'saadparwaiz1/cmp_luasnip'},
       {'hrsh7th/cmp-nvim-lsp'},
