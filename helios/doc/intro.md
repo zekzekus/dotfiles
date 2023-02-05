c80bacab5cc945a0be027a63b9b483a268e07c88
Zekeriya Koc <zekzekus@gmail.com>
2023-02-05 22:22:19 +0300

    initial commit

---
+++ helios/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to helios
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ helios/test/dev/zeko/helios_test.clj
@@ -0,0 +1,7 @@
+(ns dev.zeko.helios-test
+  (:require [clojure.test :refer :all]
+            [dev.zeko.helios :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
