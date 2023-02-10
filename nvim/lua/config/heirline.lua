14a7c70a266f4a692aa0ce0a9a47fd2199e19214
Zekeriya Koc <zekzekus@gmail.com>
2023-02-10 17:01:06 +0300

    NVIM: remove winbar for some buftypes and filetypes

--- nvim/lua/config/heirline.lua
+++ nvim/lua/config/heirline.lua
@@ -114,6 +114,21 @@ local Winbar = {
   Context,
 }
 
+vim.api.nvim_create_autocmd("User", {
+    pattern = 'HeirlineInitWinbar',
+    callback = function(args)
+        local buf = args.buf
+        local buftype = vim.tbl_contains(
+            { "prompt", "nofile", "help", "quickfix" },
+            vim.bo[buf].buftype
+        )
+        local filetype = vim.tbl_contains({ "gitcommit", "fugitive" }, vim.bo[buf].filetype)
+        if buftype or filetype then
+            vim.opt_local.winbar = nil
+        end
+    end,
+})
+
 heirline.setup({
   statusline = {Statusline},
   winbar = {Winbar},
