a4157243867cc72ce719dfeae688dfe396db2b37
Zekeriya Koc <zekzekus@gmail.com>
2023-02-18 12:33:06 +0300

    NVIM: moved functions to a module

--- nvim/lua/config/heirline.lua
+++ nvim/lua/config/heirline.lua
@@ -1,9 +1,7 @@
 local heirline = prequire('heirline')
 local conditions = prequire('heirline.conditions')
 local utils = prequire('heirline.utils')
-local common = prequire('hardline.common')
-local main_theme = prequire('hardline.themes.default')
-local fmt = string.format
+local p = prequire('config.heirline.parts')
 
 local vim = vim
 
@@ -11,80 +9,23 @@ if not heirline then
   return
 end
 
-local function get_filename()
-  return vim.fn.expand('%:~:.')
-end
-
-local function get_dirname()
-  return vim.fn.fnamemodify(vim.fn.getcwd(), ':~')
-end
-
-local function get_readonly()
-  if vim.bo.readonly then
-    return '[RO]'
-  end
-  return ''
-end
-
-local function get_modified()
-  if vim.bo.modified then
-    return '[+]'
-  end
-  if not vim.bo.modifiable then
-    return '[-]'
-  end
-  return ''
-end
-
-local create_part = function(item_fn, class)
-  return {
-    provider = function()
-      local item = item_fn()
-      if item == '' then
-        return item
-      end
-      return fmt(' %s ', item_fn())
-    end,
-    init = function(self)
-      self.mode = common.modes[vim.fn.mode()] or common.modes['?']
-    end,
-    hl = function(self)
-      local state = 'active'
-      if class == 'mode' then
-        state = self.mode.state
-      end
-      if conditions.is_active() then
-        return {
-          fg = main_theme[class][state]['guifg'],
-          bg = main_theme[class][state]['guibg'],
-        }
-      else
-        return {
-          fg = main_theme[class]['inactive']['guifg'],
-          bg = main_theme[class]['inactive']['guibg'],
-        }
-      end
-    end
-  }
-end
-
-local FileInfo = create_part(get_filename, 'warning')
-local Readonly = create_part(get_readonly, 'warning')
-local Modified = create_part(get_modified, 'warning')
+local FileInfo = p.create_part(p.get_filename, 'warning')
+local Readonly = p.create_part(p.get_readonly, 'warning')
+local Modified = p.create_part(p.get_modified, 'warning')
 
 local Align = { provider = "%=" }
-local Mode = create_part(require('hardline.parts.mode').get_item, 'mode')
-local Git = create_part(require('hardline.parts.git').get_item, 'high')
+local Mode = p.create_part(require('hardline.parts.mode').get_item, 'mode')
+local Git = p.create_part(require('hardline.parts.git').get_item, 'high')
 local Filename = { FileInfo, Readonly, Modified }
-local Dirname = create_part(get_dirname, 'med')
-local WordCount = create_part(require('hardline.parts.wordcount').get_item, 'med')
-local LspError = create_part(require('hardline.parts.lsp').get_error, 'warning')
-local LspWarning = create_part(require('hardline.parts.lsp').get_warning, 'warning')
-local Whitespace = create_part(require('hardline.parts.whitespace').get_item, 'warning')
-local Filetype = create_part(require('hardline.parts.filetype').get_item, 'high')
-local Lines = create_part(require('hardline.parts.line').get_item, 'warning')
-local ListInfos = create_part(listinfos, 'warning')
-local Context = create_part(require('hardline.parts.treesitter-context').get_item, 'warning')
+local Dirname = p.create_part(p.get_dirname, 'med')
+local WordCount = p.create_part(require('hardline.parts.wordcount').get_item, 'med')
+local LspError = p.create_part(require('hardline.parts.lsp').get_error, 'warning')
+local LspWarning = p.create_part(require('hardline.parts.lsp').get_warning, 'warning')
+local Whitespace = p.create_part(require('hardline.parts.whitespace').get_item, 'warning')
+local Filetype = p.create_part(require('hardline.parts.filetype').get_item, 'high')
+local Lines = p.create_part(require('hardline.parts.line').get_item, 'warning')
+local ListInfos = p.create_part(listinfos, 'warning')
+local Context = p.create_part(require('hardline.parts.treesitter-context').get_item, 'warning')
 
 local Statusline = {
   Mode,
@@ -120,36 +61,7 @@ local Winbar = {
 
 local TablineFileName = {
   provider = function(self)
-    local buflist = vim.fn.tabpagebuflist(self.tabnr)
-    local winnr = vim.fn.tabpagewinnr(self.tabnr)
-    local bufnr = buflist[winnr]
-
-    local file = vim.fn.bufname(bufnr)
-    local buftype = vim.fn.getbufvar(bufnr, '&buftype')
-    local filetype = vim.fn.getbufvar(bufnr, '&filetype')
-
-    local retval = ""
-    if buftype == 'help' then
-      retval = 'help:' .. vim.fn.fnamemodify(file, ':t:r')
-    elseif buftype == 'quickfix' then
-      retval = 'quickfix'
-    elseif filetype == 'TelescopePrompt' then
-      retval = 'Telescope'
-    elseif filetype == 'git' then
-      retval = 'Git'
-    elseif filetype == 'fugitive' then
-      retval = 'Fugitive'
-    elseif file:sub(file:len()-2, file:len()) == 'FZF' then
-      retval = 'FZF'
-    elseif buftype == 'terminal' then
-      local _, mtch = string.match(file, "term:(.*):(%a+)")
-      retval = mtch ~= nil and mtch or vim.fn.fnamemodify(vim.env.SHELL, ':t')
-    elseif file == '' then
-      retval = '[No Name]'
-    else
-      retval = vim.fn.pathshorten(vim.fn.fnamemodify(file, ':p:~:t'))
-    end
-    return fmt(" %s ", retval)
+    return p.get_tabline_filename(self.tabnr)
   end,
   hl = function(self)
     return { bold = self.is_active or self.is_visible, italic = true }
---
+++ nvim/lua/config/heirline/parts.lua
@@ -0,0 +1,100 @@
+local common = prequire('hardline.common')
+local conditions = prequire('heirline.conditions')
+local main_theme = prequire('hardline.themes.default')
+local fmt = string.format
+
+local M = {}
+
+local vim = vim
+
+M.get_filename = function()
+  return vim.fn.expand('%:~:.')
+end
+
+M.get_tabline_filename = function(tabnr)
+  local buflist = vim.fn.tabpagebuflist(tabnr)
+  local winnr = vim.fn.tabpagewinnr(tabnr)
+  local bufnr = buflist[winnr]
+
+  local file = vim.fn.bufname(bufnr)
+  local buftype = vim.fn.getbufvar(bufnr, '&buftype')
+  local filetype = vim.fn.getbufvar(bufnr, '&filetype')
+
+  local retval = ""
+  if buftype == 'help' then
+    retval = 'help:' .. vim.fn.fnamemodify(file, ':t:r')
+  elseif buftype == 'quickfix' then
+    retval = 'quickfix'
+  elseif filetype == 'TelescopePrompt' then
+    retval = 'Telescope'
+  elseif filetype == 'git' then
+    retval = 'Git'
+  elseif filetype == 'fugitive' then
+    retval = 'Fugitive'
+  elseif file:sub(file:len()-2, file:len()) == 'FZF' then
+    retval = 'FZF'
+  elseif buftype == 'terminal' then
+    local _, mtch = string.match(file, "term:(.*):(%a+)")
+    retval = mtch ~= nil and mtch or vim.fn.fnamemodify(vim.env.SHELL, ':t')
+  elseif file == '' then
+    retval = '[No Name]'
+  else
+    retval = vim.fn.pathshorten(vim.fn.fnamemodify(file, ':p:~:t'))
+  end
+  return fmt(" %s ", retval)
+end
+
+M.get_dirname = function()
+  return vim.fn.fnamemodify(vim.fn.getcwd(), ':~')
+end
+
+M.get_readonly = function()
+  if vim.bo.readonly then
+    return '[RO]'
+  end
+  return ''
+end
+
+M.get_modified = function()
+  if vim.bo.modified then
+    return '[+]'
+  end
+  if not vim.bo.modifiable then
+    return '[-]'
+  end
+  return ''
+end
+
+M.create_part = function(item_fn, class)
+  return {
+    provider = function()
+      local item = item_fn()
+      if item == '' then
+        return item
+      end
+      return fmt(' %s ', item_fn())
+    end,
+    init = function(self)
+      self.mode = common.modes[vim.fn.mode()] or common.modes['?']
+    end,
+    hl = function(self)
+      local state = 'active'
+      if class == 'mode' then
+        state = self.mode.state
+      end
+      if conditions.is_active() then
+        return {
+          fg = main_theme[class][state]['guifg'],
+          bg = main_theme[class][state]['guibg'],
+        }
+      else
+        return {
+          fg = main_theme[class]['inactive']['guifg'],
+          bg = main_theme[class]['inactive']['guibg'],
+        }
+      end
+    end
+  }
+end
+
+return M
