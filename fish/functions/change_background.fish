073b36673a2d483317e04cce02e3d2e1adac427d
Zekeriya Koc <zekzekus@gmail.com>
2023-04-29 21:02:58 +0300

    FISH: added a function to update nvim theme

---
+++ fish/functions/change_background.fish
@@ -0,0 +1,48 @@
+function change_background --argument mode_setting
+  # change background to the given mode. If mode is missing, 
+  # we try to deduct it from the system settings.
+
+  set -l mode "light" # default value
+  if test -z $mode_setting
+    set -l val (defaults read -g AppleInterfaceStyle) >/dev/null
+    if test $status -eq 0
+      set mode "dark"
+    end
+  else
+    switch $mode_setting
+      case light
+        osascript -l JavaScript -e "Application('System Events').appearancePreferences.darkMode = false" >/dev/null
+        set mode "light"
+      case dark
+        osascript -l JavaScript -e "Application('System Events').appearancePreferences.darkMode = true" >/dev/null
+        set mode "dark"
+    end
+  end
+
+  # change neovim
+  for addr in (/opt/homebrew/bin/nvr --serverlist)
+    switch $mode
+      case dark
+        /opt/homebrew/bin/nvr --servername "$addr" -c "set background=dark"
+      case light
+        /opt/homebrew/bin/nvr --servername "$addr" -c "set background=light"
+    end
+    /opt/homebrew/bin/nvr --servername "$addr" -c "lua require('heirline').reset_highlights()"
+  end
+
+  # change tmux
+  # switch $mode
+  #   case dark
+  #     tmux source-file ~/.tmux/tmux-dark.conf
+  #   case light
+  #     tmux source-file ~/.tmux/tmux-light.conf
+  # end
+
+  # change alacritty
+  # switch $mode
+  #   case dark
+  #     alacritty-theme gruvbox_dark
+  #   case light
+  #     alacritty-theme gruvbox_light
+  # end
+end
