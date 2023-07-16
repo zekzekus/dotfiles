1e82d575baa4f0fdbd881d4892b3a73d9dad6a43
Zekeriya Koc <zekzekus@gmail.com>
2023-07-16 20:50:10 +0300

    NVIM: upgraded neotree to 3* version

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -82,7 +82,7 @@ packer.startup(function(use)
 
   use {
     "nvim-neo-tree/neo-tree.nvim",
-      branch = "v2.x",
+      branch = "v3.x",
       requires = {
         "nvim-lua/plenary.nvim",
         "kyazdani42/nvim-web-devicons",
