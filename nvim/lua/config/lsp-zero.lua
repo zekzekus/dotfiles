d62536d31384ea49f3fb2f60511afb7122e8f8da
Zekeriya Koc <zekzekus@gmail.com>
2023-08-13 09:07:47 +0300

    NVIM: added keymap to select completion with enter

--- nvim/lua/config/lsp-zero.lua
+++ nvim/lua/config/lsp-zero.lua
@@ -88,6 +88,12 @@ end)
 
 lsp.setup()
 
+cmp.setup({
+  mapping = {
+    ['<CR>'] = cmp.mapping.confirm({select = false}),
+  }
+})
+
 cmp.event:on(
   'confirm_done',
   cmp_autopairs.on_confirm_done()
