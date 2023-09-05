eecbadf9bad36369f11d1aefee5c9db792179111
Zekeriya Koc <zekzekus@gmail.com>
2023-09-05 14:25:46 +0300

    NVIM: add lazy.nvim and migrate basic plugins

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,21 @@
 require("utils")
-require("plugins")
+-- require("plugins")
+require("config.packer")
 require("options")
 require("keybindings")
 vim.cmd("source ~/.config/nvim/viml.vim")
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup("plugins")
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,119 +1,113 @@
-local packer = prequire("config.packer")
+return {
+  'LionC/nest.nvim',
+  'christoomey/vim-tmux-navigator',
+  'tpope/vim-sleuth',
+  'tpope/vim-repeat',
+  'tpope/vim-surround',
+  'tpope/vim-unimpaired',
+  'tpope/vim-fugitive',
+  'tpope/vim-commentary',
+  'tpope/vim-projectionist',
+  'tpope/vim-dispatch',
+  'radenling/vim-dispatch-neovim',
 
-if not packer then
-  return
-end
+  {'mcchrish/zenbones.nvim',
+   dependencies = { 'rktjmp/lush.nvim' },
+   lazy = false,
+   priority = 1000 },
+  { 'preservim/tagbar',         cmd      = { 'TagbarToggle' } },
+  { 'Shougo/junkfile.vim',      cmd      = { 'JunkfileOpen' } },
+  { 'szw/vim-smartclose',       cmd      = { 'SmartClose' } },
+  { 'AndrewRadev/linediff.vim', cmd      = { 'Linediff' } },
+  { 'tpope/vim-dadbod',         cmd      = { 'DB' } },
+  { 'gpanders/nvim-parinfer',   ft       = { 'clojure' } },
+  { 'clojure-vim/vim-jack-in',  ft       = { 'clojure' } },
+  { 'tpope/vim-fireplace',      ft       = { 'clojure' } },
+  { 'tpope/vim-jdaddy',         ft       = { 'json' } },
 
-packer.startup(function(use)
-  use 'wbthomason/packer.nvim'
-  use 'LionC/nest.nvim'
+  -- use {
+  --   'ethanholz/nvim-lastplace',
+  --   config = function() default_config('nvim-lastplace') end,
+  -- }
 
-  use 'christoomey/vim-tmux-navigator'
-  use 'tpope/vim-sleuth'
-  use 'tpope/vim-repeat'
-  use 'tpope/vim-surround'
-  use 'tpope/vim-unimpaired'
-  use 'tpope/vim-fugitive'
-  use 'tpope/vim-commentary'
-  use 'tpope/vim-projectionist'
-  use 'tpope/vim-dispatch'
-  use 'radenling/vim-dispatch-neovim'
+  {'ellisonleao/glow.nvim',
+   cmd = { 'Glow' },
+   config = function() default_config('glow') end,
+  },
 
-  use { 'mcchrish/zenbones.nvim',   requires = { 'rktjmp/lush.nvim' } }
-  use { 'preservim/tagbar',         cmd      = { 'TagbarToggle' } }
-  use { 'Shougo/junkfile.vim',      cmd      = { 'JunkfileOpen' } }
-  use { 'szw/vim-smartclose',       cmd      = { 'SmartClose' } }
-  use { 'AndrewRadev/linediff.vim', cmd      = { 'Linediff' } }
-  use { 'tpope/vim-dadbod',         cmd      = { 'DB' } }
-  use { 'gpanders/nvim-parinfer',   ft       = { 'clojure' } }
-  use { 'clojure-vim/vim-jack-in',  ft       = { 'clojure' } }
-  use { 'tpope/vim-fireplace',      ft       = { 'clojure' } }
-  use { 'tpope/vim-jdaddy',         ft       = { 'json' } }
+  -- use {
+  --   "rebelot/heirline.nvim",
+  --   config = function() require('config.heirline') end,
+  --   requires = { 'ojroques/nvim-hardline' },
+  -- }
 
-  use {
-    'ethanholz/nvim-lastplace',
-    config = function() default_config('nvim-lastplace') end,
-  }
+  -- use {
+  --   'lewis6991/gitsigns.nvim',
+  --   config = function() default_config('gitsigns') end,
+  -- }
 
-  use {
-    'ellisonleao/glow.nvim',
-    cmd = { 'Glow' },
-    config = function() default_config('glow') end,
-  }
+  -- use {
+  --   'nvim-telescope/telescope.nvim',
+  --   requires = {
+  --     {'nvim-lua/popup.nvim'},
+  --     {'nvim-lua/plenary.nvim'},
+  --     {'fcying/telescope-ctags-outline.nvim'},
+  --     {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' },
+  --   },
+  --   config = function() require('config.telescope') end,
+  --   cmd = { 'Telescope' },
+  -- }
 
-  use {
-    "rebelot/heirline.nvim",
-    config = function() require('config.heirline') end,
-    requires = { 'ojroques/nvim-hardline' },
-  }
+  -- use {
+  --   'scalameta/nvim-metals',
+  --   requires = { "nvim-lua/plenary.nvim" },
+  -- }
 
-  use {
-    'lewis6991/gitsigns.nvim',
-    config = function() default_config('gitsigns') end,
-  }
+  -- use {
+  --   'nvim-treesitter/nvim-treesitter',
+  --   config = function() require('config.treesitter') end,
+  --   run = { ':TSUpdate' },
+  -- }
 
-  use {
-    'nvim-telescope/telescope.nvim',
-    requires = {
-      {'nvim-lua/popup.nvim'},
-      {'nvim-lua/plenary.nvim'},
-      {'fcying/telescope-ctags-outline.nvim'},
-      {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' },
-    },
-    config = function() require('config.telescope') end,
-    cmd = { 'Telescope' },
-  }
+  -- use {
+  --   "SmiteshP/nvim-navic",
+  --   requires = "neovim/nvim-lspconfig",
+  -- }
 
-  use {
-    'scalameta/nvim-metals',
-    requires = { "nvim-lua/plenary.nvim" },
-  }
+  -- use {
+  --   "nvim-neo-tree/neo-tree.nvim",
+  --     branch = "v3.x",
+  --     requires = {
+  --       "nvim-lua/plenary.nvim",
+  --       "kyazdani42/nvim-web-devicons",
+  --       "MunifTanjim/nui.nvim",
+  --     },
+  --   config = function() require('config.neotree') end,
+  -- }
 
-  use {
-    'nvim-treesitter/nvim-treesitter',
-    config = function() require('config.treesitter') end,
-    run = { ':TSUpdate' },
-  }
+  -- use {
+  --   'VonHeikemen/lsp-zero.nvim',
+  --   config = function() require('config.lsp-zero') end,
+  --   requires = {
+  --     {'neovim/nvim-lspconfig'},
+  --     {'williamboman/mason.nvim'},
+  --     {'williamboman/mason-lspconfig.nvim'},
+  --     { 'nvimdev/lspsaga.nvim' },
 
-  use {
-    "SmiteshP/nvim-navic",
-    requires = "neovim/nvim-lspconfig",
-  }
+  --     {'hrsh7th/nvim-cmp'},
+  --     {'hrsh7th/cmp-path'},
+  --     {'saadparwaiz1/cmp_luasnip'},
+  --     {'hrsh7th/cmp-nvim-lsp'},
+  --     {'hrsh7th/cmp-nvim-lua'},
 
-  use {
-    "nvim-neo-tree/neo-tree.nvim",
-      branch = "v3.x",
-      requires = {
-        "nvim-lua/plenary.nvim",
-        "kyazdani42/nvim-web-devicons",
-        "MunifTanjim/nui.nvim",
-      },
-    config = function() require('config.neotree') end,
-  }
+  --     {'L3MON4D3/LuaSnip'},
+  --     {'rafamadriz/friendly-snippets'},
+  --   }
+  -- }
 
-  use {
-    'VonHeikemen/lsp-zero.nvim',
-    config = function() require('config.lsp-zero') end,
-    requires = {
-      {'neovim/nvim-lspconfig'},
-      {'williamboman/mason.nvim'},
-      {'williamboman/mason-lspconfig.nvim'},
-      { 'nvimdev/lspsaga.nvim' },
+  -- use {
+  --   'windwp/nvim-autopairs',
+  --   config = function() default_config('nvim-autopairs') end,
+  -- }
 
-      {'hrsh7th/nvim-cmp'},
-      {'hrsh7th/cmp-path'},
-      {'saadparwaiz1/cmp_luasnip'},
-      {'hrsh7th/cmp-nvim-lsp'},
-      {'hrsh7th/cmp-nvim-lua'},
-
-      {'L3MON4D3/LuaSnip'},
-      {'rafamadriz/friendly-snippets'},
-    }
-  }
-
-  use {
-    'windwp/nvim-autopairs',
-    config = function() default_config('nvim-autopairs') end,
-  }
-
-end)
+}
