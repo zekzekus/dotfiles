5896218735540514438b8aa0c11eae0dd6c8b405
Resul Alkan <me@resulalkan.com>
2023-11-26 13:44:40 +0100

    SHELL: Added pgsql commands to powershell

--- shell/profile.ps1
+++ shell/profile.ps1
@@ -2,6 +2,7 @@ $env:Path += ';C:\nvim\bin'
 $env:Path += ';C:\mingw64\bin'
 $env:Path += ';C:\cmder\bin'
 $env:Path += ';C:\ripgrep'
+$env:Path += ';C:\PostgreSQL\pgsql\bin'
 
 Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
 Set-PSReadLineKeyHandler -Key Ctrl+u -Function BackwardDeleteLine
@@ -13,3 +14,9 @@ function cddash { if ($args[0] -eq '-') { $pwd=$OLDPWD; } elseif (!$args[0]) { $
 Set-Location $pwd; } Set-Variable -Name OLDPWD -Value $tmp -Scope global; }
 
 set-alias -Name cd -value cddash -Option AllScope
+
+Function START_PGSQL {pg_ctl -D C:\PostgreSQL\pgsql\data -l logfile start}
+Function STOP_PGSQL {pg_ctl -D C:\PostgreSQL\pgsql\data -l logfile stop}
+
+set-alias -Name pgstart -Value START_PGSQL
+set-alias -Name pgstop -Value STOP_PGSQL
