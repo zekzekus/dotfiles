c481b2a52d5c10555dfb7ac52a1b1d26d0442f89
ZekeriyaKoç <zekeriyakoc@ZekeriyaKocs-MacBook-Pro.local>
2023-12-11 14:58:53 +0300

    FISH: updated some hardcoded paths

--- fish/conf.d/virtualfish-loader.fish
+++ fish/conf.d/virtualfish-loader.fish
@@ -1,4 +1,4 @@
 set -g VIRTUALFISH_VERSION 2.5.4
-set -g VIRTUALFISH_PYTHON_EXEC /Users/zekus/.asdf/installs/python/3.10.4/bin/python3.10
-source /Users/zekus/.asdf/installs/python/3.10.4/lib/python3.10/site-packages/virtualfish/virtual.fish
+set -g VIRTUALFISH_PYTHON_EXEC /Users/zekeriyakoc/.asdf/installs/python/3.12.1/bin/python3.12
+source /Users/zekeriyakoc/.asdf/installs/python/3.12.1/lib/python3.12/site-packages/virtualfish/virtual.fish
 emit virtualfish_did_setup_plugins
