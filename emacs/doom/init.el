d8beb1d9d2bee5d126c0a690de2a224722764f8c
Zekeriya Koc <zekzekus@gmail.com>
2023-12-19 12:34:00 +0300

    EMACS: switch to vterm for terminal emulation

--- emacs/doom/init.el
+++ emacs/doom/init.el
@@ -76,8 +76,8 @@
        :term
        ;;eshell            ; the elisp shell that works everywhere
        ;;shell             ; simple shell REPL for Emacs
-       term              ; basic terminal emulator for Emacs
-       ;;vterm             ; the best terminal emulation in Emacs
+       ;; term              ; basic terminal emulator for Emacs
+       vterm             ; the best terminal emulation in Emacs
 
        :checkers
        syntax
