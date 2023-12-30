80c516ce912a4fcadeaa16718c177fe283af91ba
Zekeriya Koc <zekzekus@gmail.com>
2023-12-30 21:51:15 +0300

    EMACS: set new font berkeley mono

--- emacs/doom/config.el
+++ emacs/doom/config.el
@@ -18,8 +18,8 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-(setq doom-font (font-spec :family "MonoLisa Nerd font" :size 14))
-(setq doom-variable-pitch-font (font-spec :family "MonoLisa Nerd Font" :size 14))
+(setq doom-font (font-spec :family "Berkeley Mono" :size 15))
+(setq doom-variable-pitch-font (font-spec :family "Berkeley Mono" :size 15))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
