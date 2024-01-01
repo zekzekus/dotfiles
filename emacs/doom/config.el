47df08faddffb2af645b4d69f14482b38d27c542
Zekeriya Koc <zekzekus@gmail.com>
2024-01-01 21:38:31 +0300

    EMACS: new theme and bigger font

--- emacs/doom/config.el
+++ emacs/doom/config.el
@@ -18,15 +18,15 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-(setq doom-font (font-spec :family "Berkeley Mono" :size 14))
-(setq doom-variable-pitch-font (font-spec :family "Berkeley Mono" :size 14))
+(setq doom-font (font-spec :family "Berkeley Mono" :size 15))
+(setq doom-variable-pitch-font (font-spec :family "Berkeley Mono" :size 15))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. These are the defaults.
 (load! "menguless-theme")
 
-(setq doom-theme 'parchment)
+(setq doom-theme 'nordless)
 (setq display-line-numbers-type nil)
 (setq mac-command-modifier 'meta
       mac-option-modifier 'meta)
