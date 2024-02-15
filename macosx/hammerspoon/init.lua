8a51fb35e486837bf98ebc3f02e5f424258b266f
Zekeriya Koc <zekzekus@gmail.com>
2024-02-15 22:34:17 +0300

    HAMMERSPOON: Remove Mail app from auto launch list

--- macosx/hammerspoon/init.lua
+++ macosx/hammerspoon/init.lua
@@ -192,7 +192,6 @@ hs.hotkey.bind(hyper, "a", function()
   hs.application.launchOrFocus("iTerm")
   hs.application.launchOrFocus("Slack")
   hs.application.launchOrFocus("Tana")
-  hs.application.launchOrFocus("Mail")
 end)
 -- hs.hotkey.bind(hyper, "r", function() hs.application.launchOrFocus("Roam Research") end)
 
