e62386cfada9eb0d23518bae4f3852fd43b517bc
Zekeriya Koc <zekzekus@gmail.com>
2024-03-15 15:11:04 +0300

    NVIM: Hopefully temporary measure for telescope bug on redwaring the dropdown theme selector

--- nvim/lua/keybindings.lua
+++ nvim/lua/keybindings.lua
@@ -3,8 +3,8 @@ local nest = prequire('nest')
 local files = {
   name = 'Files',
   prefix = '<leader>f',
-  { 'f', [[<cmd>Telescope find_files theme=dropdown previewer=false<cr>]] },
-  { 'j', [[<cmd>Telescope find_files theme=dropdown cwd=~/.cache/junkfile prompt_title=Junkfiles<cr>]] },
+  { 'f', [[<cmd>Telescope find_files previewer=false<cr>]] },
+  { 'j', [[<cmd>Telescope find_files cwd=~/.cache/junkfile prompt_title=Junkfiles<cr>]] },
   { 's', [[<cmd>w<cr>]] },
   { 'W', [[<cmd>%s/\s\+$//<cr>:let @/=''<cr>]] },
   { 't', [[<cmd>Neotree left toggle<cr>]] },
@@ -20,7 +20,7 @@ local floatings = {
 local buffers = {
   name = 'Buffers',
   prefix = '<leader>',
-  { 'bb',     [[<cmd>Telescope buffers theme=dropdown previewer=false<cr>]] },
+  { 'bb',     [[<cmd>Telescope buffers previewer=false<cr>]] },
   { 'bd',     [[<cmd>bdelete<cr>]] },
   { '<tab>', [[<cmd>b#<cr>]] },
   { '`',     [[<cmd>b#<cr>]] },
@@ -38,7 +38,7 @@ local search = {
 local others = {
   name = 'Others',
   prefix = '<leader>',
-  { '<space>', [[<cmd>Telescope find_files theme=dropdown previewer=false<cr>]] },
+  { '<space>', [[<cmd>Telescope find_files previewer=false<cr>]] },
   { 'qq',      [[<cmd>SmartClose<cr>]] },
   { 'V',       'V`]' },
 }
