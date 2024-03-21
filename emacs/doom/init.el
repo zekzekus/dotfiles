4269fbd08f901e923014fbb327a1e302fd4a5212
Zekeriya Koc <zekzekus@gmail.com>
2024-03-21 11:02:19 +0300

    EMACS: use new completion enginer called corfu

--- emacs/doom/init.el
+++ emacs/doom/init.el
@@ -22,7 +22,10 @@
        ;;layout            ; auie,ctsrnm is the superior home row
 
        :completion
-       company           ; the ultimate code completion backend
+       ;; company           ; the ultimate code completion backend
+       (corfu
+        +icons
+        +orderless)  ; complete with cap(f), cape and a flying feather!
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
        ;;ivy               ; a search engine for love and life
