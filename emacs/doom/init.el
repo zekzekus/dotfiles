456da7b2b446f49a61f7f80173d2984ac6d2a7e5
Zekeriya Koc <zekzekus@gmail.com>
2024-03-25 13:07:32 +0300

    EMACS: Lets use mature choice for now

--- emacs/doom/init.el
+++ emacs/doom/init.el
@@ -22,10 +22,11 @@
        ;;layout            ; auie,ctsrnm is the superior home row
 
        :completion
-       ;; company           ; the ultimate code completion backend
-       (corfu
-        +icons
-        +orderless)  ; complete with cap(f), cape and a flying feather!
+       (company
+        +childframe)           ; the ultimate code completion backend
+       ;; (corfu
+       ;;  +icons
+       ;;  +orderless)  ; complete with cap(f), cape and a flying feather!
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
        ;;ivy               ; a search engine for love and life
