609298fa9db7c92aa35ab78c16f1f79d4ce66c6e
Zekeriya Koc <zekzekus@gmail.com>
2024-07-10 10:44:30 +0200

    EMACS: Update the font name for doom config

--- emacs/doom/config.el
+++ emacs/doom/config.el
@@ -18,8 +18,8 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-(setq doom-font (font-spec :family "Berkeley Mono" :size 15))
-(setq doom-variable-pitch-font (font-spec :family "Berkeley Mono" :size 15))
+(setq doom-font (font-spec :family "Berkeley Mono Nerd Complete" :size 15))
+(setq doom-variable-pitch-font (font-spec :family "Berkeley Mono Nerd Complete" :size 15))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
