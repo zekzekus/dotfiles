5fedd807315f1a7c74c6f8359e875dc1fd6682ac
Zekeriya Koc <zekzekus@gmail.com>
2024-07-30 16:09:11 +0200

    HAMMERSPOON: Add Things app to auto launch list

--- macosx/hammerspoon/init.lua
+++ macosx/hammerspoon/init.lua
@@ -192,6 +192,7 @@ hs.hotkey.bind(hyper, "a", function()
   hs.application.launchOrFocus("iTerm")
   hs.application.launchOrFocus("Slack")
   hs.application.launchOrFocus("Logseq")
+  hs.application.launchOrFocus("Things")
 end)
 -- hs.hotkey.bind(hyper, "r", function() hs.application.launchOrFocus("Roam Research") end)
 
