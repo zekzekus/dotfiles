d5bd4dd72078bc0a268111d5887c626f3ee3ccb7
Zekeriya Koc <zekzekus@gmail.com>
2024-09-14 08:58:46 +0200

    NVIM: Let's try this avante plugin for AI assistance

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -108,5 +108,40 @@ return {
       'nvim-telescope/telescope.nvim',
     },
     config = true
-  }
+  },
+
+  {
+    'yetone/avante.nvim',
+    event = 'VeryLazy',
+    lazy = false,
+    version = false,
+    config = function() require('config.avante') end,
+    opts = { },
+    build = 'make',
+    dependencies = {
+      'stevearc/dressing.nvim',
+      'nvim-lua/plenary.nvim',
+      'MunifTanjim/nui.nvim',
+
+      'nvim-tree/nvim-web-devicons',
+      -- 'zbirenbaum/copilot.lua',
+      {
+        'HakonHarnes/img-clip.nvim',
+        event = 'VeryLazy',
+        opts = {
+          embed_image_as_base64 = false,
+          prompt_for_file_name = false,
+          drag_and_drop = { insert_mode = true, },
+          use_absolute_path = true,
+        },
+      },
+      {
+        'MeanderingProgrammer/render-markdown.nvim',
+        opts = {
+          file_types = { 'markdown', 'Avante' },
+        },
+        ft = { 'markdown', 'Avante' },
+      },
+    },
+  },
 }
