8d0c7120505215c1d3085f5549e63453f528923c
Zekeriya Koc <zekzekus@gmail.com>
2024-12-16 12:35:31 +0100

    FISH: Add ocaml tool opam to path and set it up

--- fish/config.fish
+++ fish/config.fish
@@ -57,3 +57,12 @@ fish_add_path /opt/homebrew/opt/llvm/bin
 fish_add_path /opt/homebrew/opt/mysql-client@5.7/bin
 
 set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin /Users/zekeriyakoc/.ghcup/bin $PATH # ghcup-env
+
+
+# BEGIN opam configuration
+# This is useful if you're using opam as it adds:
+#   - the correct directories to the PATH
+#   - auto-completion for the opam binary
+# This section can be safely removed at any time if needed.
+test -r '/Users/zekeriyakoc/.opam/opam-init/init.fish' && source '/Users/zekeriyakoc/.opam/opam-init/init.fish' > /dev/null 2> /dev/null; or true
+# END opam configuration
