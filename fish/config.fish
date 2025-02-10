330c38d2e2afffa586135eaf52161dfd3d6840e3
Zekeriya Koc <zekzekus@gmail.com>
2025-02-10 14:24:19 +0100

    FISH: Update based on latest asdf instructions

--- fish/config.fish
+++ fish/config.fish
@@ -45,9 +45,19 @@ if command -sq starship
   starship init fish | source
 end
 
-if test -e /opt/homebrew/opt/asdf/libexec/asdf.fish
-  source /opt/homebrew/opt/asdf/libexec/asdf.fish
+# ASDF configuration code
+if test -z $ASDF_DATA_DIR
+    set _asdf_shims "$HOME/.asdf/shims"
+else
+    set _asdf_shims "$ASDF_DATA_DIR/shims"
 end
+
+# Do not use fish_add_path (added in Fish 3.2) because it
+# potentially changes the order of items in PATH
+if not contains $_asdf_shims $PATH
+    set -gx --prepend PATH $_asdf_shims
+end
+set --erase _asdf_shims
 
 if test -e ~/.asdf/plugins/java/set-java-home.fish
   source ~/.asdf/plugins/java/set-java-home.fish
