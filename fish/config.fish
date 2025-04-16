1ee668745c7ec5dbef4bb8477ac6588e75056339
Zekeriya Koc <zekzekus@gmail.com>
2025-04-16 14:19:10 +0200

    FISH: Remove redundant path exports

--- fish/config.fish
+++ fish/config.fish
@@ -1,11 +1,6 @@
 set --export PATH /opt/homebrew/bin $PATH
-set --export PATH /opt/homebrew/opt/grep/libexec/gnubin $PATH
-set --export PATH /usr/local/bin $PATH
 set --export PATH /usr/local/sbin $PATH
 set --export PATH $HOME/.local/bin $PATH
-set --export PATH $HOME/.deno/bin $PATH
-set --export PATH $HOME/.roswell/bin $PATH
-set --export PATH $HOME/Library/Application\ Support/Coursier/bin $PATH
 set --export PATH $HOME/bin $PATH
 
 if command -sq rustc
