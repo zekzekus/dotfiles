d19c136f47b78ffaede85e435167ee42d2f1b6bf
Zekeriya Koc <zekzekus@gmail.com>
2025-04-16 11:47:06 +0200

    NIX: Moved most of the env vars to home manager

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,3 @@
-set --export ZEK_DEVEL_HOME $HOME/devel/projects
-set --export ZEK_DEFAULT_PROJECT_DIR personal
-set --export ZEK_DEVEL_WORK_HOME $ZEK_DEVEL_HOME/$ZEK_DEVEL_DEFAULT_DIR
-set --export ZEK_DEVEL_PERSONAL_HOME $ZEK_DEVEL_HOME/personal
-
 set --export PATH /opt/homebrew/bin $PATH
 set --export PATH /opt/homebrew/opt/grep/libexec/gnubin $PATH
 set --export PATH /usr/local/bin $PATH
@@ -13,31 +8,11 @@ set --export PATH $HOME/.roswell/bin $PATH
 set --export PATH $HOME/Library/Application\ Support/Coursier/bin $PATH
 set --export PATH $HOME/bin $PATH
 
-set --export LANG "en_US.UTF-8"
-set --export LC_COLLATE "en_US.UTF-8"
-set --export LC_CTYPE "en_US.UTF-8"
-set --export LC_MESSAGES "en_US.UTF-8"
-set --export LC_MONETARY "en_US.UTF-8"
-set --export LC_NUMERIC "en_US.UTF-8"
-set --export LC_TIME "en_US.UTF-8"
-set --export LC_ALL "en_US.UTF-8"
-
-set --export EDITOR nvim
-set --export MANPAGER "nvim +Man!"
-
-set --export TMUX_FZF_LAUNCH_KEY "o"
-
 if command -sq rustc
   set --export RUST_SRC_PATH (rustc --print sysroot)/lib/rustlib/src/rust/library
 end
 
 alias mux='tmuxinator'
-
-set --export FZF_DEFAULT_COMMAND 'rg --files --hidden --follow --glob "!.git/*"'
-set --export FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
-set --export FZF_ALT_C_COMMAND 'bfs -type d -nohidden'
-
-set --export HOMEBREW_NO_INSTALL_CLEANUP 1
 
 if test -e ~/.nix-profile/etc/profile.d/hm-session-vars.sh
   bass source ~/.nix-profile/etc/profile.d/hm-session-vars.sh
--- home-manager/home.nix
+++ home-manager/home.nix
@@ -4,6 +4,10 @@ let
   username = "zekus";
   homeDir = "/Users/${username}";
   dotfilesDir = "${homeDir}/devel/tools/dotfiles";
+  develHome = "${homeDir}/devel/projects";
+  defaultProjectDir = "personal";
+  workHome = "${develHome}/${defaultProjectDir}";
+  personalHome = "${develHome}/personal";
 in
 {
   home.username = "${username}";
@@ -50,6 +54,30 @@ in
   };
 
   home.sessionVariables = {
+    ZEK_DEVEL_HOME = "${develHome}";
+    ZEK_DEFAULT_PROJECT_DIR  = "personal";
+    ZEK_DEVEL_WORK_HOME = "${develHome}/${defaultProjectDir}";
+    ZEK_DEVEL_PERSONAL_HOME = "${develHome}/personal";
+
+    LANG = "en_US.UTF-8";
+    LC_COLLATE = "en_US.UTF-8";
+    LC_CTYPE = "en_US.UTF-8";
+    LC_MESSAGES = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+    LC_ALL = "en_US.UTF-8";
+
+    EDITOR = "nvim";
+    MANPAGER = "nvim +Man!";
+
+    TMUX_FZF_LAUNCH_KEY = "o";
+    FZF_DEFAULT_COMMAND = "rg --files --hidden --follow --glob \"!.git/*\"";
+    FZF_CTRL_T_COMMAND = "rg --files --hidden --follow --glob \"!.git/*\"";
+    FZF_ALT_C_COMMAND = "bfs -type d -nohidden";
+    FZF_DEFAULT_OPTS = "--style full";
+
+    HOMEBREW_NO_INSTALL_CLEANUP = 1;
   };
 
   # Let Home Manager install and manage itself.
