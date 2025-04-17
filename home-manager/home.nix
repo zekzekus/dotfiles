a7c7174e022115654a178c97dc1ae02c60a8b2af
Zekeriya Koc <zekzekus@gmail.com>
2025-04-17 13:19:47 +0200

    NIX: Move fish and starship config to home manager

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -16,7 +16,7 @@ in
   home.stateVersion = "24.11";
 
   home.packages = with pkgs; [
-    # files waiting: tmux, fish
+    # files waiting: tmux
     neovim
 
     ripgrep
@@ -31,9 +31,6 @@ in
 
     tmux
     extract_url
-
-    fish
-    starship
 
     python313
     python313Packages.pip
@@ -93,6 +90,38 @@ in
     HOMEBREW_NO_INSTALL_CLEANUP = 1;
   };
 
-  # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
+
+  programs.fish = {
+    enable = true;
+    shellAliases = {
+      mux = "tmuxinator";
+    };
+    plugins = 
+    [
+      {
+        name = "nix-env";
+        src = pkgs.fetchFromGitHub {
+          owner = "lilyball";
+          repo = "nix-env.fish";
+          rev = "7b65bd228429e852c8fdfa07601159130a818cfa";
+          sha256 = "sha256-RG/0rfhgq6aEKNZ0XwIqOaZ6K5S4+/Y5EEMnIdtfPhk=";
+        };
+      }
+      {
+        name = "z";
+        src = pkgs.fetchFromGitHub {
+          owner = "jethrokuan";
+          repo = "z";
+          rev = "067e867debee59aee231e789fc4631f80fa5788e";
+          sha256 = "sha256-emmjTsqt8bdI5qpx1bAzhVACkg0MNB/uffaRjjeuFxU=";
+        };
+      }
+    ];
+  };
+
+  programs.starship = {
+    enable = true;
+    enableFishIntegration = true;
+  };
 }
