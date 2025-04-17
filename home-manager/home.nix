80e757a9773ad1fe01f7653455aa0ff59d42c051
Zekeriya Koc <zekzekus@gmail.com>
2025-04-17 18:13:28 +0200

    NIX: Move all tmux config into home manager

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -16,7 +16,6 @@ in
   home.stateVersion = "24.11";
 
   home.packages = with pkgs; [
-    # files waiting: tmux
     neovim
 
     ripgrep
@@ -28,15 +27,11 @@ in
     git
     tig
 
-    tmux
-    extract_url
-
     python313
     python313Packages.pip
     poetry
 
     ruby_3_4
-    tmuxinator
 
     nodejs_23
     clojure
@@ -154,5 +149,42 @@ in
     settings = {
       cmd_duration.disabled = true;
     };
+  };
+
+  programs.tmux = {
+    enable = true;
+    tmuxinator.enable = true;
+    shell = "${pkgs.fish}/bin/fish";
+    escapeTime = 0;
+    prefix = "C-a";
+    baseIndex = 0;
+    mouse = true;
+    terminal = "xterm-ghostty";
+    keyMode = "vi";
+    plugins = with pkgs.tmuxPlugins; [
+      sensible
+      vim-tmux-navigator
+      urlview
+      extrakto
+      tmux-fzf
+    ];
+    extraConfig = ''
+set -g default-command "${pkgs.fish}/bin/fish"
+set -g visual-activity on
+setw -g pane-base-index 1
+setw -g monitor-activity on
+unbind C-b
+bind C-s send-prefix
+bind C-l send-keys 'C-l'
+bind C-h send-keys 'C-h'
+bind C-a last-window
+bind-key Escape copy-mode
+bind-key p paste-buffer
+bind-key -T copy-mode-vi 'v' send -X begin-selection
+bind-key -T copy-mode-vi 'V' send -X select-line
+bind-key -T copy-mode-vi 'r' send -X rectangle-toggle
+bind-key -T copy-mode-vi 'y' send -X copy-pipe-and-cancel "pbcopy"
+if-shell "test -f ${dotfilesDir}/tmux/tmuxline.gruvbox.conf" "source ${dotfilesDir}/tmux/tmuxline.gruvbox.conf"
+    '';
   };
 }
