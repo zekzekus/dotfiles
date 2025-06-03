8aa9c07d9aa3289925fcac3a59876b96be699ef5
Zekeriya Koc <zekzekus@gmail.com>
2025-06-03 12:00:44 +0200

    NIX: Add default keybindings for aerospace

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -208,6 +208,60 @@ in
 
     aerospace = {
       enable = true;
+      userSettings = {
+        gaps = {
+          inner.horizontal = 5;
+          inner.vertical = 5;
+          outer.left = 5;
+          outer.bottom = 5;
+          outer.top = 5;
+          outer.right = 5;
+        };
+        mode.main.binding = {
+          alt-slash = "layout tiles horizontal vertical";
+          alt-comma = "layout accordion horizontal vertical";
+
+          alt-h = "focus left";
+          alt-j = "focus down";
+          alt-k = "focus up";
+          alt-l = "focus right";
+
+          alt-shift-h = "move left";
+          alt-shift-j = "move down";
+          alt-shift-k = "move up";
+          alt-shift-l = "move right";
+
+          alt-minus = "resize smart -50";
+          alt-equal = "resize smart +50" ;
+
+          alt-1 = "workspace 1";
+          alt-2 = "workspace 2";
+          alt-3 = "workspace 3";
+          alt-4 = "workspace 4";
+          alt-5 = "workspace 5";
+
+          alt-shift-1 = "move-node-to-workspace 1";
+          alt-shift-2 = "move-node-to-workspace 2";
+          alt-shift-3 = "move-node-to-workspace 3";
+          alt-shift-4 = "move-node-to-workspace 4";
+          alt-shift-5 = "move-node-to-workspace 5";
+
+          alt-tab = "workspace-back-and-forth";
+          alt-shift-tab = "move-workspace-to-monitor --wrap-around next";
+          alt-shift-semicolon = "mode service";
+        };
+        mode.service.binding = {
+          esc = ["reload-config" "mode main"];
+          r = ["flatten-workspace-tree" "mode main"];
+          f = ["layout floating tiling" "mode main"];
+          backspace = ["close-all-windows-but-current" "mode main"];
+
+          alt-shift-h = ["join-with left" "mode main"];
+          alt-shift-j = ["join-with down" "mode main"];
+          alt-shift-k = ["join-with up" "mode main"];
+          alt-shift-l = ["join-with right" "mode main"];
+        };
+      };
     };
 
     sketchybar = {
