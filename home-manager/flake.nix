23461eecf46ee291fe81ecce8c0f0c08138e460c
Zekeriya Koc <zekzekus@gmail.com>
2025-09-07 11:51:06 +0200

    NIX: Remove emacs and neovim nightly

--- home-manager/flake.nix
+++ home-manager/flake.nix
@@ -7,23 +7,16 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
   };
 
-  outputs = { nixpkgs, home-manager, neovim-nightly-overlay, ... }:
+  outputs = { nixpkgs, home-manager, ... }:
     let
       system = "aarch64-darwin";
       pkgs = nixpkgs.legacyPackages.${system};
-      overlays = [
-        neovim-nightly-overlay.overlays.default
-      ];
     in {
       homeConfigurations."zekus" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
         modules = [
-          {
-            nixpkgs.overlays = overlays;
-          }
           ./home.nix
         ];
       };
--- home-manager/modules/packages/default.nix
+++ home-manager/modules/packages/default.nix
@@ -3,7 +3,7 @@
 with pkgs; [
   # editors
   neovim
-  emacs
+  # emacs
 
   # terminal tools
   bfs
