69efba33eec32f591472887065d3cc2e84411ace
Zekeriya Koc <zekzekus@gmail.com>
2025-10-26 22:52:09 +0300

    EMACS: Add some flags to nix module

--- emacs/doom/init.el
+++ emacs/doom/init.el
@@ -156,7 +156,9 @@
        lua               ; one-based indices? one-based indices
        markdown          ; writing docs for people to ignore
        ;;nim               ; python + lisp at the speed of c
-       nix               ; I hereby declare "nix geht mehr!"
+       (nix
+        +lsp
+        +treesitter)               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
        (org              ; organize your plain life in plain text
         ;; +dragndrop       ; drag & drop files/images into org buffers
