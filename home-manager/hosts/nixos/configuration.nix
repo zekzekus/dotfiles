829f389fd32646bd7f6e21d31554070f293f07d0
Zekeriya Koc <zekzekus@gmail.com>
2025-10-29 16:45:23 +0300

    Add gnumake to system packages

--- home-manager/hosts/nixos/configuration.nix
+++ home-manager/hosts/nixos/configuration.nix
@@ -105,6 +105,7 @@
   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   git
   unzip
+  gnumake
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
