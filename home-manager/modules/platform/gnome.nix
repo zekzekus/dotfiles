c347b95a6e3d614926eaf9bd6114207d91ed2616
Zekeriya Koc <zekzekus@gmail.com>
2025-11-09 22:35:12 +0300

    GNOME: Some new bindings

--- home-manager/modules/platform/gnome.nix
+++ home-manager/modules/platform/gnome.nix
@@ -14,9 +14,24 @@
     };
     "org/gnome/shell/keybindings" = {
       toggle-overview = [ "<Super>space" ];
+      switch-to-application-1 = [];
+      switch-to-application-2 = [];
+      switch-to-application-3 = [];
+      switch-to-application-4 = [];
+      switch-to-application-5 = [];
     };
     "org/gnome/desktop/wm/keybindings" = {
       close = ["<Super>q"];
+      switch-to-workspace-1 = ["<Super>1"];
+      switch-to-workspace-2 = ["<Super>2"];
+      switch-to-workspace-3 = ["<Super>3"];
+      switch-to-workspace-4 = ["<Super>4"];
+      switch-to-workspace-5 = ["<Super>5"];
+      move-to-workspace-1 = ["<Super><Shift>1"];
+      move-to-workspace-2 = ["<Super><Shift>2"];
+      move-to-workspace-3 = ["<Super><Shift>3"];
+      move-to-workspace-4 = ["<Super><Shift>4"];
+      move-to-workspace-5 = ["<Super><Shift>5"];
     };
   };
 }
