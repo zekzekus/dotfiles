fc7d539adaceff6c0b1ec424a9013b9299edbd57
Zekeriya Koc <zekzekus@gmail.com>
2025-11-25 14:48:28 +0300

    Add firefox default profile

--- home-manager/modules/programs/firefox.nix
+++ home-manager/modules/programs/firefox.nix
@@ -2,4 +2,15 @@
 
 {
   enable = true;
+  profiles = {
+    default = {
+      id = 0;
+      name = "default";
+      isDefault = true;
+      
+      settings = {
+        "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+      };
+    };
+  };
 }
