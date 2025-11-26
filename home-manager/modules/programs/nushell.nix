9316bbfb4b2a071b5139e1243e99eedd3f251836
Zekeriya Koc <zekzekus@gmail.com>
2025-11-27 01:52:16 +0300

    :wRevert "Add duplicated environment variables for nushell"
    This reverts commit 67af154443109f84d0aa0160770de3ff539df475.

--- home-manager/modules/programs/nushell.nix
+++ home-manager/modules/programs/nushell.nix
@@ -7,20 +7,4 @@
    pkgs.nushellPlugins.gstat 
    pkgs.nushellPlugins.formats 
   ];
-  environmentVariables = {
-    ZEK_DEVEL_HOME = common.develHome;
-    ZEK_DEFAULT_PROJECT_DIR = common.defaultProjectDir;
-    ZEK_DEVEL_WORK_HOME = common.workHome;
-    ZEK_DEVEL_PERSONAL_HOME = common.personalHome;
-
-    EDITOR = "nvim";
-    MANPAGER = "nvim +Man!";
-
-    TMUX_FZF_LAUNCH_KEY = "o";
-    FZF_DEFAULT_COMMAND = "rg --files --hidden --follow --glob \"!.git/*\"";
-    FZF_CTRL_T_COMMAND = "rg --files --hidden --follow --glob \"!.git/*\"";
-    FZF_ALT_C_COMMAND = "bfs -type d -nohidden";
-
-    PNPM_HOME = "$HOME/.local/share/pnpm";
-  };
 }
