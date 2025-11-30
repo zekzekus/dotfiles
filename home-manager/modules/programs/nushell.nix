faca8cb64513bc800f32ef85022b6d0d2d613fac
Zekeriya Koc <zekzekus@gmail.com>
2025-12-01 00:19:10 +0300

    Cleanup and refine nushell configuration

--- home-manager/modules/programs/nushell.nix
+++ home-manager/modules/programs/nushell.nix
@@ -17,48 +17,19 @@
 
     PNPM_HOME = "${common.homeDir}/.local/share/pnpm";
   };
+  settings = {
+    show_banner = false;
+    completions = {
+      algorithm = "fuzzy";
+    };
+    error_style = "plain";
+    table = {
+      mode = "reinforced";
+    };
+  };
+
   extraEnv = ''
     $env.PATH = ($env.PATH | split row (char esep) | prepend '${common.homeDir}/.local/share/pnpm')
     $env.PATH = ($env.PATH | split row (char esep) | prepend '${common.homeDir}/bin')
-
-    $env.config.completions.algorithm = "fuzzy"
-    $env.config.completions.quick = true   # Auto-select if only one option
-    $env.config.completions.partial = true # Show partial completions
-
-    let fish_completer = {|spans|
-      # This effectively passes the cursor position and command to fish
-      ${pkgs.fish}/bin/fish --command $'complete "--do-complete=($spans | str join " ")"'
-      | from tsv --flexible --noheaders --no-infer
-      | rename value description
-    }
-
-    let carapace_completer = {|spans|
-      carapace $spans.0 nushell ...$spans | from json
-    }
-
-    let external_completer = {|spans|
-      let expanded_alias = scope aliases | where name == $spans.0 | get -o 0.expansion
-
-      let spans = if $expanded_alias != null {
-        $spans | skip 1 | prepend ($expanded_alias | split row ' ' | take 1)
-      } else {
-        $spans
-      }
-
-      match $spans.0 {
-        # Use Fish for tools where it shines
-        asdf => $fish_completer
-        mvn => $fish_completer
-        
-        # Default to Carapace for everything else
-        _ => $carapace_completer
-      } | do $in $spans
-    }
-
-    $env.config.completions.external = {
-      enable: true
-      max_results: 100
-      completer: $external_completer
-    }
   '';
 }
