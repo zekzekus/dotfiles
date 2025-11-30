a2f75934cbddddb0ba14fe96d900a71cb9f2747d
Zekeriya Koc <zekzekus@gmail.com>
2025-11-30 13:01:17 +0300

    Some styling tweaks

--- home-manager/hosts/nixos/default.nix
+++ home-manager/hosts/nixos/default.nix
@@ -81,15 +81,4 @@
       };
     };
   };
-
-  # Host-specific configuration for nixos
-  #
-  # This file is for overrides specific to this machine only.
-  # Common Linux settings are in platforms/linux.nix
-  # System-level NixOS config is in configuration.nix
-  #
-  # Examples:
-  #   home.packages = with pkgs; [ virt-manager ];
-  #   programs.git.userEmail = "nixos@example.com";
-  #   home.sessionVariables.NIXOS_MACHINE = "true";
 }
