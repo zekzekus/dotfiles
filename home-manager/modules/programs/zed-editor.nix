9a973aed70bedb95d10ca4f135bdd8cae0dd7499
Zekeriya Koc <zekzekus@gmail.com>
2025-11-30 16:30:15 +0300

    ZED: Set font size

--- home-manager/modules/programs/zed-editor.nix
+++ home-manager/modules/programs/zed-editor.nix
@@ -1,7 +1,9 @@
-{ ... }: {
+{ ... }:
+{
   enable = true;
   userSettings = {
     buffer_font_family = "TX-02";
+    buffer_font_size = 13;
     vim_mode = true;
     theme = {
       mode = "system";
