22a4c73ecad0245e3c9ce4fe421f1771da02cd0b
Zekeriya Koc <zekzekus@gmail.com>
2025-11-30 16:40:53 +0300

    ZED: Set font size and line height

--- home-manager/modules/programs/zed-editor.nix
+++ home-manager/modules/programs/zed-editor.nix
@@ -3,7 +3,8 @@
   enable = true;
   userSettings = {
     buffer_font_family = "TX-02";
-    buffer_font_size = 13;
+    buffer_font_size = 15;
+    buffer_line_height = "standard";
     vim_mode = true;
     theme = {
       mode = "system";
