3a6c74c0f4eb05f4e5aab7701d86dad5e16f005c
Zekeriya Koc <zekzekus@gmail.com>
2025-12-01 17:55:08 +0300

    NUSHELL: Add external completers to use fish completions

--- home-manager/modules/programs/nushell.nix
+++ home-manager/modules/programs/nushell.nix
@@ -32,4 +32,43 @@
     $env.PATH = ($env.PATH | split row (char esep) | prepend '${common.homeDir}/.local/share/pnpm')
     $env.PATH = ($env.PATH | split row (char esep) | prepend '${common.homeDir}/bin')
   '';
+
+  extraConfig = ''
+    let fish_completer = {|spans|
+      # This effectively passes the cursor position and command to fish
+      ${pkgs.fish}/bin/fish --command $'complete "--do-complete=($spans | str join " ")"'
+      | from tsv --flexible --noheaders --no-infer
+      | rename value description
+    }
+
+    let carapace_completer = {|spans|
+      carapace $spans.0 nushell ...$spans | from json
+    }
+
+    let external_completer = {|spans|
+      let expanded_alias = scope aliases | where name == $spans.0 | get -o 0.expansion
+
+      let spans = if $expanded_alias != null {
+        $spans | skip 1 | prepend ($expanded_alias | split row ' ' | take 1)
+      } else {
+        $spans
+      }
+
+      match $spans.0 {
+        # Use Fish for tools where it shines
+        asdf => $fish_completer
+        mvn => $fish_completer
+        tailscale => $fish_completer
+        
+        # Default to Carapace for everything else
+        _ => $carapace_completer
+      } | do $in $spans
+    }
+
+    $env.config.completions.external = {
+      enable: true
+      max_results: 100
+      completer: $external_completer
+    }
+  '';
 }
