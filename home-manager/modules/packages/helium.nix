1c7d4526994be87850ccd637b6e94fade0c08f78
Zekeriya Koc <zekzekus@gmail.com>
2025-12-14 00:54:42 +0300

    Update helium browser

--- home-manager/modules/packages/helium.nix
+++ home-manager/modules/packages/helium.nix
@@ -2,11 +2,11 @@
 
 let
   pname = "helium";
-  version = "0.7.3.1";
+  version = "0.7.4.1";
 
   src = pkgs.fetchurl {
     url = "https://github.com/imputnet/helium-linux/releases/download/${version}/helium-${version}-x86_64.AppImage";
-    hash = "sha256-rYxAOGgjEP7/LZS3z3C3XodsV+TkDl3p3VbdSozHFfY=";
+    hash = "sha256-JFaGuRbjNEzFOEpntbzARxCOxA/2Fxhd31nXaVPfpFg=";
   };
 
   appimageContents = pkgs.appimageTools.extractType2 {
