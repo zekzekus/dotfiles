5cfd65656eef1b25256d4ceee0404268c311e1fe
Zekeriya Koc <zekzekus@gmail.com>
2026-02-21 12:17:40 +0300

    Set font size for zed editor

--- home-manager/modules/programs/zed-editor.nix
+++ home-manager/modules/programs/zed-editor.nix
@@ -3,7 +3,7 @@
     enable = true;
     userSettings = {
       buffer_font_family = "TX-02";
-      buffer_font_size = 16;
+      buffer_font_size = 18;
       buffer_line_height = "standard";
       vim_mode = true;
       theme = {
