04f0b05300e3f64561ce00285172398bb9d3bdea
Zekeriya Koc <zekzekus@gmail.com>
2026-03-12 17:58:15 +0300

    Update flake inputs and noctalia settings

--- nix/modules/packages/helium.nix
+++ nix/modules/packages/helium.nix
@@ -1,10 +1,10 @@
 {pkgs}: let
   pname = "helium";
-  version = "0.9.4.1";
+  version = "0.10.2.1";
 
   src = pkgs.fetchurl {
     url = "https://github.com/imputnet/helium-linux/releases/download/${version}/helium-${version}-x86_64.AppImage";
-    hash = "sha256-N5gdWuxOrIudJx/4nYo4/SKSxakpTFvL4zzByv6Cnug=";
+    hash = "sha256-Kh6UgdleK+L+G4LNiQL2DkQIwS43cyzX+Jo6K0/fX1M=";
   };
 
   appimageContents = pkgs.appimageTools.extractType2 {
