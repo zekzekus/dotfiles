2b25ba6e4739ea5e9120114b615e8f7ca1b144e3
Zekeriya Koc <zekzekus@gmail.com>
2026-03-15 03:02:57 +0300

    Update flake inputs and helium browser

--- nix/modules/packages/helium.nix
+++ nix/modules/packages/helium.nix
@@ -1,10 +1,10 @@
 {pkgs}: let
   pname = "helium";
-  version = "0.10.2.1";
+  version = "0.10.5.1";
 
   src = pkgs.fetchurl {
     url = "https://github.com/imputnet/helium-linux/releases/download/${version}/helium-${version}-x86_64.AppImage";
-    hash = "sha256-Kh6UgdleK+L+G4LNiQL2DkQIwS43cyzX+Jo6K0/fX1M=";
+    hash = "sha256-c/ea8C1XjTkBo0/ujGHEbKWyCmRMxyuiuOzAO9AMf1o=";
   };
 
   appimageContents = pkgs.appimageTools.extractType2 {
