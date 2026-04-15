121bc4ca112b57046cad0da18608a855973116da
Zekeriya Koc <zekzekus@gmail.com>
2026-04-15 14:49:22 +0300

    Update flake inputs and helium browser

--- flake.nix
+++ flake.nix
@@ -69,6 +69,29 @@
     overlays = [
       neovim-nightly-overlay.overlays.default
       (_: prev: {
+        neovim-unwrapped = prev.neovim-unwrapped.overrideAttrs (old: {
+          postInstall =
+            (old.postInstall or "")
+            + ''
+              mkdir -p $out/share/applications
+              if [ ! -f $out/share/applications/nvim.desktop ]; then
+                cat > $out/share/applications/nvim.desktop <<EOF
+              [Desktop Entry]
+              Name=Neovim
+              GenericName=Text Editor
+              TryExec=nvim
+              Exec=nvim %F
+              Type=Application
+              Keywords=Text;editor;
+              Icon=nvim
+              Categories=Utility;TextEditor;
+              StartupNotify=false
+              MimeType=text/english;text/plain;text/x-makefile;text/x-c++hdr;text/x-c++src;text/x-chdr;text/x-csrc;text/x-java;text/x-moc;text/x-pascal;text/x-tcl;text/x-tex;application/x-shellscript;text/x-c;text/x-c++;
+              Terminal=true
+              EOF
+              fi
+            '';
+        });
         python3Packages = prev.python3Packages.overrideScope (
           _: pyPrev: {
             picosvg = pyPrev.picosvg.overridePythonAttrs {
