0a64756210cf349a3b8f905979e8e86ae980a5f5
Zekeriya Koc <zekzekus@gmail.com>
2026-04-20 16:44:17 +0300

    Update helium browser

--- nix/modules/packages/helium.nix
+++ nix/modules/packages/helium.nix
@@ -1,10 +1,10 @@
 {pkgs}: let
   pname = "helium";
-  version = "0.11.2.1";
+  version = "0.11.3.2";
 
   src = pkgs.fetchurl {
     url = "https://github.com/imputnet/helium-linux/releases/download/${version}/helium-${version}-x86_64.AppImage";
-    hash = "sha256-tGOgJSCGrGfkG2aE0VcGm2GH8ttiBQ602GftlWEHRHA=";
+    hash = "sha256-5gdyKg12ZV2hpf0RL+eoJnawuW/J8NobiG+zEA0IOHA=";
   };
 
   appimageContents = pkgs.appimageTools.extractType2 {
