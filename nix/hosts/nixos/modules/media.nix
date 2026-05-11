bcc48f5e733a3dff3bc5f7742e36e9c545c8dddf
Zekeriya Koc <zekzekus@gmail.com>
2026-05-11 20:12:38 +0300

    Add alternatives for mpv, imv and zathura

--- nix/hosts/nixos/modules/media.nix
+++ nix/hosts/nixos/modules/media.nix
@@ -1,38 +1,27 @@
-_: {
-  programs = {
-    mpv = {
-      enable = true;
-      config = {
-        hwdec = "auto-safe";
-        vo = "gpu-next";
-        gpu-context = "wayland";
-        profile = "high-quality";
-      };
-    };
-
-    imv.enable = true;
-
-    zathura = {
-      enable = true;
-      options = {
-        selection-clipboard = "clipboard";
-        recolor = true;
-      };
-    };
-  };
+{pkgs, ...}: {
+  home.packages = with pkgs; [
+    haruna
+    loupe
+    papers
+  ];
 
   xdg.configFile."mimeapps.list".force = true;
   xdg.mimeApps = {
     enable = true;
     defaultApplications = {
-      "image/png" = "imv.desktop";
-      "image/jpeg" = "imv.desktop";
-      "image/gif" = "imv.desktop";
-      "image/webp" = "imv.desktop";
-      "image/tiff" = "imv.desktop";
-      "image/bmp" = "imv.desktop";
-      "image/svg+xml" = "imv.desktop";
-      "application/pdf" = "org.pwmt.zathura.desktop";
+      "image/png" = "org.gnome.Loupe.desktop";
+      "image/jpeg" = "org.gnome.Loupe.desktop";
+      "image/gif" = "org.gnome.Loupe.desktop";
+      "image/webp" = "org.gnome.Loupe.desktop";
+      "image/tiff" = "org.gnome.Loupe.desktop";
+      "image/bmp" = "org.gnome.Loupe.desktop";
+      "image/svg+xml" = "org.gnome.Loupe.desktop";
+      "application/pdf" = "org.gnome.Papers.desktop";
+      "video/mp4" = "org.kde.haruna.desktop";
+      "video/x-matroska" = "org.kde.haruna.desktop";
+      "video/webm" = "org.kde.haruna.desktop";
+      "video/quicktime" = "org.kde.haruna.desktop";
+      "video/x-msvideo" = "org.kde.haruna.desktop";
     };
   };
 }
