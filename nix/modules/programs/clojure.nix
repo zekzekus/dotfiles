09f6d681a3f0266a2957394dd8b01284965fdb1e
Zekeriya Koc <zekzekus@gmail.com>
2026-05-18 21:08:05 +0300

    Patch practicalli clojure config to pin nrepl

--- nix/modules/programs/clojure.nix
+++ nix/modules/programs/clojure.nix
@@ -2,14 +2,27 @@
   pkgs,
   practicalli-clojure-cli-config,
   ...
-}: {
+}: let
+  # nrepl >= 1.3 passes a non-string port to rebel-readline.main, which
+  # crashes inside tools.cli (Integer cannot be cast to CharSequence).
+  # Pin nrepl to 1.2.0 only inside Practicalli's rebel-launching aliases.
+  patched-deps = pkgs.runCommand "practicalli-deps-patched" {} ''
+    cp ${practicalli-clojure-cli-config}/deps.edn $out
+    chmod u+w $out
+    ${pkgs.gnused}/bin/sed -i -E '
+      /^[[:space:]]*:repl\/(rebel|reloaded)[[:space:]]*$/,/^[[:space:]]*$/ {
+        s|nrepl/nrepl[[:space:]]+\{:mvn/version "1\.7\.0"\}|nrepl/nrepl {:mvn/version "1.2.0"}|
+      }
+    ' $out
+  '';
+in {
   home.packages = with pkgs; [
     clojure
     clojure-lsp
   ];
 
   xdg.configFile = {
-    "clojure/deps.edn".source = "${practicalli-clojure-cli-config}/deps.edn";
+    "clojure/deps.edn".source = patched-deps;
     "clojure/rebel_readline.edn".source = "${practicalli-clojure-cli-config}/rebel_readline.edn";
     "clojure/.cljstyle".source = "${practicalli-clojure-cli-config}/.cljstyle";
   };
