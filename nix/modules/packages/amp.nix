4ec6816bf14a68038a81bc505ad378e4fbcc8163
Zekeriya Koc <zekzekus@gmail.com>
2026-08-04 14:24:47 +0300

    Update flake inputs

--- nix/modules/packages/amp.nix
+++ nix/modules/packages/amp.nix
@@ -2,25 +2,25 @@
   stdenv,
   fetchurl,
 }: let
-  version = "0.0.1785775571-g90a48e";
+  version = "0.0.1785833597-ge3f010";
 
   # Per-system source: (platform, sha256). Pattern is parsed by scripts/update-amp.
   sources = {
     x86_64-linux = {
       platform = "linux-x64";
-      sha256 = "27ee657665acbc9f60f5793bd96de583effd503160bebce1319ab3add5587557";
+      sha256 = "34987921c33fa58c018ef62ba61dc58a62f31e6e72bce279dfc675238bc0fd02";
     };
     aarch64-linux = {
       platform = "linux-arm64";
-      sha256 = "2386e8a4ff2605cf4a08cbf59af6e3c7e1fff57dd7ea02040c9581c7fa6cd39b";
+      sha256 = "0f596f4d89c6f66237a1abbb63963754d8093cd89788af2a9f9fe9b65d7437a3";
     };
     x86_64-darwin = {
       platform = "darwin-x64";
-      sha256 = "886dca1adf7b3c9d4a563e556f5c4ce6e0bdc3c9e628c152e9ab9bf64dbf8ae8";
+      sha256 = "15d25f49d283f6d96deb635fc3b84548c71a4045caddcde66fbe16036bfba0e9";
     };
     aarch64-darwin = {
       platform = "darwin-arm64";
-      sha256 = "9c5e512f368c089b840b38e952a2f1ff98f457b1ad6ce83dbbb0e51aeb7a2752";
+      sha256 = "23eba136193d5dc506e6bf5b5185cd171bde36e6f2d3c66abf903755bb6f2a62";
     };
   };
 
