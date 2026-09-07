076c76867cf2009f01dee85bb0de234b095c98ab
Zekeriya Koc <zekzekus@gmail.com>
2026-09-07 22:29:26 +0300

    Set Berkelet mono for zed terminal buffers

--- nix/profiles/graphical/linux/zed-editor.nix
+++ nix/profiles/graphical/linux/zed-editor.nix
@@ -5,6 +5,7 @@
       buffer_font_family = "TX-02";
       buffer_font_size = 18;
       buffer_line_height = "standard";
+      terminal.font_family = "TX-02";
       vim_mode = true;
       theme = {
         mode = "system";
